2a41823f054778b0d06eaaa6fb3221fc661237be
mul14 <mul14.net@gmail.com>
2011-11-30 10:27:47 +0700

    Tambah Satker model

--- application/models/msatker.php
+++ application/models/msatker.php
@@ -1,2 +1,16 @@
 <?php
-class Msatker
+class Msatker extends CI_Model
+{
+    /**
+     * @param $cs Nomer CS (A, B, C, D atau E)
+     * @return Integer
+     */
+    public function antrian_terakhir($cs)
+    {
+        $sql = "SELECT MAX(number) number FROM `antrian` WHERE cs='{$cs}'";
+        $result = $this->db->query($sql);
+        $temp = $result->result();
+        $temp = $temp[0];
+        return $temp->number;
+    }
+}
