2318abbed576d5e7c1fb9da12268e3780d8e9b56
mul14 <mul14.net@gmail.com>
2011-11-30 13:18:32 +0700

    Antrian sudah bisa naik angkanya

--- application/models/msatker.php
+++ application/models/msatker.php
@@ -13,4 +13,17 @@ class Msatker extends CI_Model
         $temp = $temp[0];
         return $temp->number;
     }
+
+    /**
+     * Naikin angka antrian
+     *
+     * @param $cs Nomer CS (A, B, C, D atau E)
+     * @return void
+     */
+    public function plusone($cs)
+    {
+        $cs = strtoupper($cs);
+        $antrian_terakhir = $this->antrian_terakhir($cs) + 1;
+        $this->db->query("INSERT INTO `antrian` (number, cs) VALUES ('{$antrian_terakhir}', '{$cs}')");
+    }
 }
