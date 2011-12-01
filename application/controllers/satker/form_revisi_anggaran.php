25dfdadb87757410af28ea9a86d44c7d6a9a0aea
mul14 <mul14.net@gmail.com>
2011-12-01 20:45:16 +0700

    Mengaktifkan fungsi form pengajuan revisi anggaran

--- application/controllers/satker/form_revisi_anggaran.php
+++ application/controllers/satker/form_revisi_anggaran.php
@@ -2,7 +2,7 @@
 class Form_revisi_anggaran extends CI_Controller
 {
 
-    function Form_revisi_anggaran()
+    function __construct()
     {
         parent::__construct();
     }
@@ -23,6 +23,57 @@ class Form_revisi_anggaran extends CI_Controller
               $this->load->view('login');
           }*/
     }
-}
 
-?>
+    public function add_revisi_anggaran()
+    {
+        extract($_POST); // Lazy coder use this (_ _"). Help me to fix this
+
+        // TODO: Belum ditambahin form validation
+
+        // tb_formulir_revisi
+        $sql = 'INSERT INTO tb_formulir_revisi
+                (id_formulir, id_satker, no_tiket_frontdesk, tanggal)
+                VALUES (NULL, ?, ?, NOW())';
+        $this->db->query($sql, array($id_satker, 2));
+        $id_formulir = $this->db->query('SELECT id_formulir FROM tb_formulir_revisi ORDER BY tanggal DESC LIMIT 1');
+        $id_formulir = ($id_formulir->result());
+        $id_formulir = $id_formulir[0]->id_formulir;
+
+
+        // tb_tiket_frontdesk
+        $sql = "INSERT INTO tb_tiket_frontdesk (id_satker, id_formulir) VALUES (?, ?)";
+        $this->db->query($sql, array($id_satker, $id_formulir));
+        $no_tiket_frontdesk = $this->db->query("SELECT no_tiket_frontdesk FROM tb_tiket_frontdesk ORDER BY no_tiket_frontdesk DESC LIMIT 1");
+        $no_tiket_frontdesk = $no_tiket_frontdesk->result();
+        $no_tiket_frontdesk = $no_tiket_frontdesk[0];
+        $no_tiket_frontdesk = $no_tiket_frontdesk->no_tiket_frontdesk;
+
+
+        // tb_histori_tiket
+        $sql = "INSERT INTO tb_histori_tiket (no_tiket_frontdesk, id_user, tanggal) VALUES (?, ?, NOW());";
+        $this->db->query($sql, array($no_tiket_frontdesk, null));
+
+
+        // tb_kelengkapan_formulir
+        if (isset($kelengkapan)) {
+            foreach ($kelengkapan as $value) {
+                $sql = "INSERT INTO tb_kelengkapan_formulir (no_tiket_frontdesk, id_kelengkapan) VALUES (?, ?)";
+                $this->db->query($sql, array($no_tiket_frontdesk, $value));
+            }
+        }
+
+        $this->session->set_flashdata('msg', 'Data telah masuk');
+        redirect('/satker/form_revisi_anggaran');
+
+    }
+
+    public function search_satker()
+    {
+        $id_satker = $this->input->post('id_satker');
+
+        if ($id_satker >= 3 AND is_numeric($id_satker)) {
+            $result = $this->db->query("SELECT * FROM `tb_satker` WHERE `id_satker` LIKE '%{$id_satker}' LIMIT 30");
+            var_dump($result->result());
+        }
+    }
+}
--- application/views/satker/form_revisi_anggaran.php
+++ application/views/satker/form_revisi_anggaran.php
@@ -1,95 +1,118 @@
+<script type="text/javascript " src="<?php echo base_url() . 'js/jquery-1.4.2.min.js' ?>"></script>
+<script type="text/javascript">
+    jQuery(function() {
+        $('#id_satker').keyup(function() {
+            var id_satker = $(this).val();
+            setTimeout(function() {
+                console.log(id_satker);
+                $.post('<?php echo site_url('/satker/form_revisi_anggaran/search_satker') ?>', id_satker, function(response) {
+                    console.log(response);
+                });
+            }, 2000);
+
+        })
+    });
+</script>
+
+<?php if ($this->session->flashdata('msg')): ?>
+<div id="notice" style="padding: 4px; background: #ffffaa; text-align: center;">
+    <?php echo $this->session->flashdata('msg') ?>
+</div>
+<?php endif ?>
+
 <div id="konten">
     <div style="display: none;" id="tab1" class="tab_konten">
 
         <h2 style="font-weight:bold; color:#000; text-align:center; ">FORMULIR ISIAN PENGAJUAN REVISI ANGGARAN</h2>
         <br/>
 
-        <div class="table">
-            <div id="head">
-                <span style="margin:-5px 0px 0px -1420px; padding-left:10px; position:absolute; width:80px; height:10px; background:#FFF;">Data Satker</span>
+        <form method="post" action="<?php echo site_url('/satker/form_revisi_anggaran/add_revisi_anggaran') ?>">
+            <div class="table">
+                <div id="head">
+                    <span style="margin:-5px 0px 0px -1420px; padding-left:10px; position:absolute; width:80px; height:10px; background:#FFF;">Data Satker</span>
 
-                <form id="cari_unit" action="man_unit_cari"
-                      style="border: 1px solid #999; padding: 13px 30px 13px 13px; margin:5px 0px 0px 20px; width:96%; margin:0px 20px 0px 20px; ">
-                    <table style="margin:10px; text-align:left; font-size:10px; ">
-                        <tr>
-                            <td>Tanggal Surat Pengajuan</td>
-                            <td>10 November 2011</td>
-                        </tr>
-                        <tr>
-                            <td>Kode Satker</td>
-                            <td><input type="text" value="93091" size="100"/></td>
-                        </tr>
-                        <tr>
-                            <td>Nama Satker</td>
-                            <td><input type="text" value="Direktorat Jenderal Keuangan" size="100" disabled="disabled"/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Nama Petugas</td>
-                            <td><input type="text" value="" size="100"/></td>
-                        </tr>
-                        <tr>
-                            <td>Jabatan Petugas</td>
-                            <td><input type="text" value="" size="100"/></td>
-                        </tr>
-                        <tr>
-                            <td>No HP</td>
-                            <td><input type="text" value="" size="100"/></td>
-                        </tr>
-                    </table>
-                </form>
+                    <div id="cari_unit" action="man_unit_cari"
+                         style="border: 1px solid #999; padding: 13px 30px 13px 13px; margin:5px 0px 0px 20px; width:96%; margin:0px 20px 0px 20px; ">
+                        <table style="margin:10px; text-align:left; font-size:10px; ">
+                            <tr>
+                                <td>Tanggal Surat Pengajuan</td>
+                                <td>10 November 2011</td>
+                            </tr>
+                            <tr>
+                                <td>Kode Satker</td>
+                                <td><input type="text" validation="" id="id_satker" name="id_satker" value=""
+                                           placeholder="Masukkan Kode Satker" size="100"/></td>
+                            </tr>
+                            <tr>
+                                <td>Nama Satker</td>
+                                <td><input type="text" id="nama_satker" value="" size="100" disabled="disabled"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Nama Petugas</td>
+                                <td><input type="text" name="nama_petugas" value="" size="100"/></td>
+                            </tr>
+                            <tr>
+                                <td>Jabatan Petugas</td>
+                                <td><input type="text" name="jabatan" value="" size="100"/></td>
+                            </tr>
+                            <tr>
+                                <td>No HP</td>
+                                <td><input type="text" name="no_hp" value="" size="100"/></td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+
+                <div id="tail">
+                    <span style="margin:-5px 0px 0px -1420px; padding-left:10px; position:absolute; width:80px; height:10px; background:#FFF;">Data Satker</span>
+
+                    <div id="cari_unit" action="man_unit_cari"
+                         style="border: 1px solid #999; padding: 13px 30px 13px 13px; margin:5px 0px 0px 20px; width:96%; margin:0px 20px 0px 20px; ">
+                        <table style="margin:10px; text-align:left; font-size:10px; ">
+                            <tr>
+                                <td><input id="kelengkapan1" name="kelengkapan[]" type="checkbox" value="1" /></td>
+                                <td><label for="kelengkapan1">Surat Pengajuan Revisi ANggaran</label></td>
+                            </tr>
+                            <tr>
+                                <td><input id="kelengkapan2" name="kelengkapan[]" type="checkbox" value="2" /></td>
+                                <td><label for="kelengkapan2">SP-RKA-K/L Sebelum Revisi</label></td>
+                            </tr>
+                            <tr>
+                                <td><input id="kelengkapan3" name="kelengkapan[]" type="checkbox" value="3" /></td>
+                                <td><label for="kelengkapan3">Konsep Usulan Revisi SP-RKA-K/L</label></td>
+                            </tr>
+                            <tr>
+                                <td><input id="kelengkapan4" name="kelengkapan[]" type="checkbox" value="4" /></td>
+                                <td><label for="kelengkapan4">Arsip Data Komputer RKA-K/L hasil revisi</label></td>
+                            </tr>
+                            <tr>
+                                <td><input id="kelengkapan5" name="kelengkapan[]" type="checkbox" value="5" /></td>
+                                <td><label for="kelengkapan5">TOR</label></td>
+                            </tr>
+                            <tr>
+                                <td><input id="kelengkapan6" name="kelengkapan[]" type="checkbox" value="6"/></td>
+                                <td><label for="kelengkapan6">RAB</label></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2"><input type="text" placeholder=".........." size="120"/></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2"><input type="text" placeholder=".........." size="120"/></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2"><input type="text" placeholder=".........." size="120"/></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2"><input type="text" placeholder=".........." size="120"/></td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
             </div>
+            <input type="reset" value="reset" style="width:60px; height:25px; font-size:10px; float:left; margin-left:20px; margin-right:10px;"/>
+            <input type="submit" value="kirim" style="width:60px; height:25px; font-size:10px; float:none;"/>
 
-            <div id="tail">
-                <span style="margin:-5px 0px 0px -1420px; padding-left:10px; position:absolute; width:80px; height:10px; background:#FFF;">Data Satker</span>
-
-                <form id="cari_unit" action="man_unit_cari"
-                      style="border: 1px solid #999; padding: 13px 30px 13px 13px; margin:5px 0px 0px 20px; width:96%; margin:0px 20px 0px 20px; ">
-                    <table style="margin:10px; text-align:left; font-size:10px; ">
-                        <tr>
-                            <td><input type="checkbox"/></td>
-                            <td>Surat Pengajuan Revisi ANggaran</td>
-                        </tr>
-                        <tr>
-                            <td><input type="checkbox"/></td>
-                            <td>SP-RKA-K/L Sebelum Revisi</td>
-                        </tr>
-                        <tr>
-                            <td><input type="checkbox"/></td>
-                            <td>Konsep Usulan Revisi SP-RKA-K/L</td>
-                        </tr>
-                        <tr>
-                            <td><input type="checkbox"/></td>
-                            <td>Arsip Data Komputer RKA-K/L hasil revisi</td>
-                        </tr>
-                        <tr>
-                            <td><input type="checkbox"/></td>
-                            <td>TOR</td>
-                        </tr>
-                        <tr>
-                            <td><input type="checkbox"/></td>
-                            <td>RAB</td>
-                        </tr>
-                        <tr>
-                            <td colspan="2"><input type="text" value=".........." size="120"/></td>
-                        </tr>
-                        <tr>
-                            <td colspan="2"><input type="text" value=".........." size="120"/></td>
-                        </tr>
-                        <tr>
-                            <td colspan="2"><input type="text" value=".........." size="120"/></td>
-                        </tr>
-                        <tr>
-                            <td colspan="2"><input type="text" value=".........." size="120"/></td>
-                        </tr>
-                    </table>
-                </form>
-            </div>
-        </div>
-        <input type="submit" value="reset"
-               style="width:60px; height:25px; font-size:10px; float:left; margin-left:20px; margin-right:10px;"/>
-        <a href="alert_terkirim"><input type="submit" value="kirim"
-                                        style="width:60px; height:25px; font-size:10px; float:none;"/></a>
-
+        </form>
     </div>
 </div>
