9274ef384621351e15199c200ec8d8256548415d
mul14 <mul14.net@gmail.com>
2011-12-06 12:46:23 +0700

    Pindahkan backup dari dashboard ke menu system. Dan tambahkan sedikit style untuk tombol.

---
+++ application/views/admin/system.php
@@ -0,0 +1,9 @@
+<h1>System Administration</h1>
+
+<div id="container">
+    <div id="content">
+
+        <a class="button blue-pill" href="<?php echo site_url('admin/dashboard/database_backup') ?>">Database Backup</a>
+
+    </div>
+</div>
