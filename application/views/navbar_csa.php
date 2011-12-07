d60d12878e9f768c09ae67b52f621bf3569dea7e
mul14 <mul14.net@gmail.com>
2011-12-07 17:42:17 +0700

    Highlight current primary navigation on every page

--- application/views/navbar_csa.php
+++ application/views/navbar_csa.php
@@ -1,10 +1,29 @@
-<div id="logout"><?php echo anchor("login/process_logout", 'Logout') ?> &nbsp; <em><?php echo date('d F Y') ?></em></div>
+<?php
+$nav_dashboard = $nav_antrian = $nav_knowledge = $nav_forum = '';
+switch ($this->uri->segment(2)) {
+    case 'identitas_satker':
+        $nav_dashboard = 'current';
+        break;
+    case 'list_antrian':
+        $nav_antrian = 'current';
+        break;
+    case 'knowledge_base':
+        $nav_knowledge = 'current';
+        break;
+    case 'man_forum':
+        $nav_forum = 'current';
+        break;
+}
+?>
+
+<div id="logout"><?php echo anchor("login/process_logout", 'Logout') ?> &nbsp; <em><?php echo date('d F Y') ?></em>
+</div>
 <div id="navbar" class="clearfloat">
     <ul class="sf-menu">
-        <li class="current "><?php echo anchor('csa/dashboard', 'Dashboard');?></li>
-        <li class="current "><?php echo anchor('csa/list_antrian', 'List Antrian');?></li>
-        <li class="current "><?php echo anchor('csa/knowledge_base', 'Knowledge Base');?></li>
-        <li class="current "><a href="<?php echo base_url() . 'upload/PMK-93.pdf' ?>" target="pdf">Referensi Peraturan</a></li>
-        <li class="current "><?php echo anchor('csa/man_forum', 'Forum');?></li>
+        <li class="<?php echo $nav_dashboard ?>"><?php echo anchor('csa/dashboard', 'Dashboard');?></li>
+        <li class="<?php echo $nav_antrian ?>"><?php echo anchor('csa/list_antrian', 'List Antrian');?></li>
+        <li class="<?php echo $nav_knowledge ?>"><?php echo anchor('csa/knowledge_base', 'Knowledge Base');?></li>
+        <li class=""><a href="<?php echo base_url() . 'upload/PMK-93.pdf' ?>" target="pdf">Referensi Peraturan</a></li>
+        <li class="<?php echo $nav_forum ?>"><?php echo anchor('csa/man_forum', 'Forum');?></li>
     </ul>
 </div>
