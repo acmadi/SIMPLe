5d13a9b4bbabe7099445bb85bc3f563f61dc5ae1
mul14 <mul14.net@gmail.com>
2011-12-14 22:54:53 +0700

    Beresin Satker lagi

---
+++ application/views/satker/success.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+    <title>Sukses</title>
+    <meta http-equiv="refresh" content="5; url=<?php echo site_url('/satker/form_revisi_anggaran')  ?>">
+
+</head>
+<body>
+    Sukses Bro!!
+</body>
+</html>
