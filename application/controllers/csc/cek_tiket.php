0e63d0d186856d84cbe7186e971c895f89b57a35
mul14 <mul14.net@gmail.com>
2011-12-15 07:25:38 +0700

    CS-C (Frontdesk) Ngeliat-liat ticket udah bisa. Tapi belum bisa ekskalasi.

--- application/controllers/csc/cek_tiket.php
+++ application/controllers/csc/cek_tiket.php
@@ -2,26 +2,33 @@
 class Cek_tiket extends CI_Controller
 {
 
-    function Cek_tiket()
+    function __construct()
     {
         parent::__construct();
     }
 
-    var $title = 'Cek Tiket';
-
     function index()
     {
-        /*
-          if ($this->session->userdata('login') == TRUE)
-          {*/
+        
+    }
+
+    function view($id)
+    {
         $data['title'] = 'Cek Tiket';
         $data['content'] = 'csc/cek_tiket';
+        $sql = "SELECT tb_tiket_frontdesk.*, tb_satker.*, tb_petugas_satker.*
+                FROM tb_tiket_frontdesk JOIN tb_satker
+                ON tb_tiket_frontdesk.id_satker = tb_satker.id_satker
+                JOIN tb_petugas_satker
+                ON tb_tiket_frontdesk.id_petugas_satker = tb_petugas_satker.id_petugas_satker
+                WHERE no_tiket_frontdesk = $id";
+        $result = $this->db->query($sql);
+        $result = $result->result();
+        $result = $result[0];
+        $data['tiket'] = $result;
+
+        print_r($result);
         $this->load->view('csc/template', $data);
-        /*}
-          else
-          {
-              $this->load->view('login');
-          }*/
     }
 }
 
