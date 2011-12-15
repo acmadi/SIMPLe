6c4d942519cc4bf0e04f9888c86cb5bf726d1aba
mul14 <mul14.net@gmail.com>
2011-12-15 13:46:14 +0700

    Tambahin Identitas di CS-A (Helpdesk)

--- application/controllers/csa/helpdesk_form_pertanyaan.php
+++ application/controllers/csa/helpdesk_form_pertanyaan.php
@@ -2,26 +2,75 @@
 class Helpdesk_form_pertanyaan extends CI_Controller
 {
 
-    function Helpdesk_form_pertanyaan()
+    function __construct()
     {
         parent::__construct();
+        $this->load->model('mknowledge');
+        $this->load->model('mhelpdesk');
     }
-
-    var $title = 'Helpdesk Form - Pertanyaan';
 
     function index()
     {
-        /*if ($this->session->userdata('login') == TRUE)
-          {*/
         $data['title'] = 'Helpdesk Form - Pertanyaan';
         $data['content'] = 'csa/helpdesk/helpdesk_form_pertanyaan';
+        $data['knowledges'] = $this->mknowledge->get_all_category();
+
+        $sql = "SELECT * FROM tb_tiket_helpdesk
+                JOIN tb_petugas_satker
+                ON tb_tiket_helpdesk.id_satker = tb_petugas_satker.id_satker
+                JOIN tb_satker
+                ON tb_tiket_helpdesk.id_satker = tb_satker.id_satker
+                WHERE no_tiket_helpdesk = ?";
+
+        $result = $this->db->query($sql, array($this->session->userdata('tiket')));
+        $result = $result->result();
+        $result = $result[0];
+
+        $data['identitas'] = $result;
+
+        print_r($result);
+
+//        $this->mknowledge->get_tiket();
+
+
         $this->load->view('csa/template', $data);
-        /*}
-          else
-          {
-              $this->load->view('login/login_view');
-          }*/
     }
+
+    function submit() {
+        $data = array();
+
+        if ($_POST) {
+            $no_tiket_helpdesk = $this->input->post('no_tiket_helpdesk');
+            $kategori_knowledge_base = $this->input->post('kategori_knowledge_base');
+            $prioritas = $this->input->post('prioritas');
+            $pertanyaan = $this->input->post('pertanyaan');
+            $description = $this->input->post('description');
+
+            $sql = "UPDATE tb_tiket_helpdesk SET
+                    prioritas = ?, pertanyaan = ?, description = ?
+                    WHERE no_tiket_helpdesk = ?";
+
+            $this->db->query($sql, array($prioritas, $pertanyaan, $description, $no_tiket_helpdesk));
+
+//            $result = $this->db->query("SELECT * FROM tb_knowledge_base WHERE id_kat_knowledge_base = '$kategori_knowledge_base'");
+
+            $result = $this->mknowledge->get_all_data_category();
+            $data['result'] = $result;
+            
+        }
+
+        $data['title'] = 'Helpdesk Form - Pertanyaan';
+        $data['content'] = 'csa/helpdesk/helpdesk_form_pertanyaan_submit';
+        $data['knowledges'] = $this->mknowledge->get_all_category();
+
+        $data['prioritas'] = $prioritas;
+        $data['pertanyaan'] = $pertanyaan;
+        $data['description'] = $description;
+
+
+        $this->load->view('csa/template', $data);
+    }
+
 }
 
 ?>
