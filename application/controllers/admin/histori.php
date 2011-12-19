3a17c415a86a7b4a909b6f0ba9fb7ef5dc22640a
Fadhli Daswir <ar_raudy@yahoo.com>
2011-12-20 02:57:40 +0700

    menu history

--- application/controllers/admin/histori.php
+++ application/controllers/admin/histori.php
@@ -5,22 +5,22 @@ class Histori extends CI_Controller
     function __construct()
     {
         parent::__construct();
+		$this->load->model('Mhistori','histori');
     }
 
     var $title = 'Histori';
 
     function index()
     {
-        /*if ($this->session->userdata('login') == TRUE)
-          {*/
+		$page		= $this->histori->get_all_history(); 
+		$pageData	= $page['query']->result();
+		$pageLink	= $page['pagination1'];
+		
+		$data		= array('result'=>$pageData,'pageLink'=>$pageLink,);
+		
         $data['title'] = 'Histori';
         $data['content'] = 'admin/histori/histori';
         $this->load->view('admin/template', $data);
-        /*}
-          else
-          {
-              $this->load->view('login/login_view');
-          }*/
     }
 }
 
--- application/views/admin/histori/histori.php
+++ application/views/admin/histori/histori.php
@@ -1,123 +1,29 @@
-<ul id="nav">
-    <li><a href="#tab1" class="active">Histori</a></li>
-</ul>
-<div class="clear"></div>
-<div id="konten">
-    <div style="display: none;" id="tab1" class="tab_konten">
+<div class="content"> 
+    <h1>Histori</h1>
+    <div style="clear: both;"></div>
 
-
-        <div class="table">
-            <div class="tail">
-                <p style="padding-top: 0px; position:absolute; padding-left: 10px;">Sejarah User</p><br/>
-
-                <div style="float:none;">
-                    <b style="font-size:12px; margin-left: 200px;">Dari Tanggal</b>
-                    <table style=" margin-left: 280px; margin-top: -21px">
-                        <tr>
-                            <td>
-                                <select style="font-size:10px; ">
-                                    <option selected="selected">Tgl</option>
-                                    <option></option>
-                                    <option></option>
-                                </select>
-                            </td>
-                            <td> -</td>
-                            <td>
-                                <select style="font-size:10px; ">
-                                    <option selected="selected">Bulan</option>
-                                    <option></option>
-                                    <option></option>
-                                </select>
-                            </td>
-                            <td> -</td>
-                            <td>
-                                <select style="font-size:10px; ">
-                                    <option selected="selected">Tahun</option>
-                                    <option></option>
-                                    <option></option>
-                                </select>
-                            </td>
-                        </tr>
-                    </table>
-
-                </div>
-                <div style="float:left; position:absolute; margin-left: 600px;">
-                    <b style="font-size: 12px; margin-top: -32px; position: absolute">Sampai Tanggal</b>
-                    <table style="margin-left: 100px; margin-top: -37px">
-                        <tr>
-                            <td>
-                                <select style="font-size:10px; ">
-                                    <option selected="selected">Tgl</option>
-                                    <option></option>
-                                    <option></option>
-                                </select>
-                            </td>
-                            <td> -</td>
-                            <td>
-                                <select style="font-size:10px; ">
-                                    <option selected="selected">Bulan</option>
-                                    <option></option>
-                                    <option></option>
-                                </select>
-                            </td>
-                            <td> -</td>
-                            <td>
-                                <select style="font-size:10px; ">
-                                    <option selected="selected">Tahun</option>
-                                    <option></option>
-                                    <option></option>
-                                </select>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-                <table id="tableOne" class="yui">
-                    <thead>
-                    <tr>
-                        <th><input type="checkbox"/></th>
-                        <th>ID Akun</th>
-                        <th>Nama Akun</th>
-                        <th>Login</th>
-                        <th>Logout</th>
-                        <th class="action">Aksi</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td><input type="checkbox"/></td>
-                        <td>001</td>
-                        <td>Adi</td>
-                        <td>10/06/2011 13:00</td>
-                        <td>10/06/2011 14:00</td>
-                        <td class="action">
-                            <span class="button_kecil"><a title="Edit" href="man_keu_daftar_brg_del.php?id=59"
-                                                          onclick='return yesOrNo()'/><img
-                                    src="<?php echo base_url(); ?>images/edit.png"
-                                    style="width:20px; height:20px; "/></a></span>
-                            <span class="button_kecil"><a title="Delete" href="man_keu_daftar_brg_up.php?id=59"><img
-                                    src="<?php echo base_url(); ?>images/delete.png" style="width:20px; height:20px; "/></a></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><input type="checkbox"/></td>
-                        <td>002</td>
-                        <td>Palam</td>
-                        <td>13/06/2011 14:30</td>
-                        <td>13/06/2011 15:22</td>
-                        <td class="action">
-                            <span class="button_kecil"><a title="Edit" href="man_keu_daftar_brg_del.php?id=59"
-                                                          onclick='return yesOrNo()'/><img
-                                    src="<?php echo base_url(); ?>images/edit.png"
-                                    style="width:20px; height:20px; "/></a></span>
-                            <span class="button_kecil"><a title="Delete" href="man_keu_daftar_brg_up.php?id=59"><img
-                                    src="<?php echo base_url(); ?>images/delete.png" style="width:20px; height:20px; "/></a></span>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-
-
+    <div id="tail">
+        <table id="tableOne" class="yui">
+            <thead>
+            <tr>
+                <th class="short">No</th>
+                <th>User</th>
+                <th>Created</th>
+                <th>Message</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php $i = 1 ?>
+            <?php foreach ($result as $item): ?>
+            <tr>
+                <td class="short"><?php echo $i++ ?></td>
+                <td><?php echo $item->user ?></td>
+                <td><?php echo $item->created ?></td>
+                <td><?php echo $item->message ?></td>
+            </tr>
+            <?php endforeach;?>
+            </tbody>
+        </table>
     </div>
+	<div class="pagination"><?php echo ($pageLink)?'Halaman '.$pageLink:'';?></div><br />
 </div>
