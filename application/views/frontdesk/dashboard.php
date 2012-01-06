77f1050cbceb7e88b951fb2bc57df7b0b33bb710
mul14 <mul14.net@gmail.com>
2012-01-06 16:08:04 +0700

    Ganti chart di dashboard frontdesk dari tufte-graph pake jQuery visualize.

--- application/views/frontdesk/dashboard.php
+++ application/views/frontdesk/dashboard.php
@@ -1,33 +1,28 @@
-<center>
-<div style="width:1010px; height:230px; border:#CCC solid 3px; margin:20% 0;">
-	<div style="width:200px; float:left; "><br />
-    	<h4>Konsultasi Sistem Penganggaran</h4><br /><br />
-        <a href="cetak_no_antrian_csa"><div id="ABCDE"><h1 class="title">A</h1></div></a><br />
-    	<b><h4>A 56</h4></b>
-    </div>
-    <div class="separator"></div>
-	<div style="width:200px; float:left; "><br />
-    	<h4>Konsultasi Aplikasi Anggaran</h4><br /><br />
-        <a href="cetak_no_antrian_csb"><div id="ABCDE"><h1 class="title">B</h1></div></a><br />
-    	<b><h4>B 48</h4></b>
-    </div>
-    <div class="separator"></div>
-	<div style="width:200px; float:left; "><br />
-    	<h4>Revisi SP-RKA-K/L</h4><br /><br />
-        <a href="cetak_no_antrian_csc"><div id="ABCDE"><h1 class="title">C</h1></div></a><br />
-    	<b><h4>C 50</h4></b>
-    </div>
-    <div class="separator"></div>
-	<div style="width:200px; float:left; "><br />
-    	<h4>Pengambilan Dokumen</h4><br /><br />
-        <a href="cetak_no_antrian_csd"><div id="ABCDE"><h1 class="title">D</h1></div></a><br />
-    	<b><h4>D 08</h4></b>
-    </div>
-    <div class="separator"></div>
-	<div style="width:200px; float:left; "><br />
-    	<h4>Saluran Pengadaan DJA</h4><br /><br />
-        <a href="cetak_no_antrian_cse"><div id="ABCDE"><h1 class="title">E</h1></div></a><br />
-    	<b><h4>E 07</h4></b>
-    </div>
+<div class="content">
+    <h1>Dashboard</h1>
+
+    <table class="chart" style="display: none;">
+        <!--<caption>Hello</caption>-->
+        <thead>
+        <tr>
+            <th scope="col">Tiket hari ini</th>
+            <th scope="col">Dokumen yang bisa diambil</th>
+            <th scope="col">Dokumen yang dikembalikan</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td scope="row">
+                <?php echo $this->db->from('tb_tiket_frontdesk')->like('tanggal', date('Y-m-d'))->get()->num_rows(); ?>
+            </td>
+            <td scope="row">
+                <?php echo $this->db->from('tb_tiket_frontdesk')->where('status', 'close')->where('is_active', 1)->get()->num_rows(); ?>
+            </td>
+            <td scope="row">
+                <?php echo $this->db->from('tb_tiket_frontdesk')->where('is_active', 3)->get()->num_rows(); ?>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+
 </div>
-</center>
