6553db58d6f27f448ae57fc8432b23b9de633bb1
mul14 <mul14.net@gmail.com>
2012-01-08 17:53:43 +0700

    Tambah edit dan keterangan di calendar

--- application/views/admin/calendar/add.php
+++ application/views/admin/calendar/add.php
@@ -25,7 +25,16 @@
     <fieldset>
         <legend>Tanggal Libur</legend>
         <form method="post" action="<?php echo site_url('/admin/calendar/add') ?>">
-            <input name="calendar" id="calendar"/>
+            <p>
+                <label style="width: 120px; display: inline-block; ">Tanggal</label>
+                <input type="text" name="calendar" id="calendar"/>
+            </p>
+
+
+            <p>
+                <label style="width: 120px; display: inline-block;">Keterangan</label>
+                <input type="text" name="keterangan"/>
+            </p>
 
             <p>
                 <input type="submit" name="submit" value="Simpan" class="button blue-pill"/>
