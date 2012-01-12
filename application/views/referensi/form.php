73684f8debe482ab272fcd883239004ac15bbbbd
Akhyar <akhyrul@gmail.com>
2012-01-12 18:02:47 +0700

    referensi peraturan udah dipermantaafff

--- application/views/referensi/form.php
+++ application/views/referensi/form.php
@@ -2,9 +2,7 @@
 
 echo form_open('referensi/search');
 
-echo '<h2>Cari Referensi Peraturan</h2>';
-echo form_input('keyword', '', 'placeholder="Ketikkan kata kunci di sini lalu tekan enter..."') . ' ' ;
-
+echo form_input('keyword', '', 'placeholder="Ketikkan kata kunci pencarian di sini lalu tekan enter..."') . ' ' ;
 echo form_submit('submit', 'Cari', 'class="button blue-pill"');
 
 echo form_close();
