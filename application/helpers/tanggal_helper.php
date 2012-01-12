24ed9543bbbfd143e62dda11a7668ea4290f8b41
mul14 <mul14.net@gmail.com>
2012-01-13 05:40:16 +0700

    Bikin helper untuk ngitung hari kerja

--- application/helpers/tanggal_helper.php
+++ application/helpers/tanggal_helper.php
@@ -28,4 +28,29 @@ function table_tanggal($datetime)
     return $temp;
 }
 
+function hari_kerja($tanggal)
+{
+    $date1 = new DateTime(date('Y-m-d H:i:s'));
+    $date2 = new DateTime($tanggal);
+    //                            $day = $date1->diff($date2);
+    $day = date_diff($date1, $date2);
+
+    $hari = $day->days;
+
+    $hari_kerja = (int)($day->days / 7) * 2;
+    $hari_kerja = $hari - $hari_kerja;
+
+    $sql = "SELECT * FROM tb_calendar WHERE holiday BETWEEN '{$tanggal}' AND NOW()";
+    $hari_kerja = $hari_kerja - $this->db->query($sql)->num_rows();
+
+    //    if ($day->days > 4) {
+    //                            echo '<span style="font-weight: bold; font-size: 13px; color: white; background: tomato; padding: 0 4px;">' . $hari_kerja . '</span>';
+    //    } elseif ($day->days == 4) {
+    //                            echo '<span style="font-weight: bold; font-size: 13px; color: yellow; background: black; padding: 0 4px;">' . $hari_kerja . '</span>';
+    //    } else {
+    //                            echo '<span style="">' . $day->days . '</span>';
+    //    }
+    return $hari_kerja;
+}
+
 ?>
