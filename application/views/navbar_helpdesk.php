baa270a8334384d8a57c2eac547f2c0a28f53a81
Akhyar <akhyrul@gmail.com>
2012-01-12 11:12:13 +0700

    benerin link knowledge base di helpdesk

--- application/views/navbar_helpdesk.php
+++ application/views/navbar_helpdesk.php
@@ -23,7 +23,7 @@ switch ($this->uri->segment(2)) {
 <ul class="sf-menu">
     <li class="<?php echo $nav_dashboard ?>"><?php echo anchor('helpdesk/dashboard', 'Dashboard');?></li>
     <li class="<?php echo $nav_identitas_satker ?>"><?php echo anchor('helpdesk/identitas_satker', 'Isi Identitas Satker');?></li>
-    <li class="<?php echo $nav_knowledge ?>"><?php echo anchor('helpdesk/knowledge_base/lists', 'Knowledge Base');?></li>
+    <li class="<?php echo $nav_knowledge ?>"><?php echo anchor('helpdesk/knowledge_base', 'Knowledge Base');?></li>
     <li class=""><a href="<?php echo base_url() . 'upload/PMK-93.pdf' ?>" target="pdf">Referensi Peraturan</a></li>
     <li class="<?php echo $nav_forum ?>"><?php echo anchor('helpdesk/man_forum', 'Forum');?></li>
 </ul>
