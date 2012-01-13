2eaad846b860c3b4065f508398cf4478224df9f6
Fadhli Daswir <ar_raudy@yahoo.com>
2012-01-13 19:20:02 +0700

    penambahan pada modul dirjen

---
+++ application/views/dirjen/frontdesk.php
@@ -0,0 +1,109 @@
+<div class="content">
+
+    <h1>Front Desk</h1>
+
+<?php if ($result->num_rows() > 0): ?>
+    <div class="table">
+        <div id="head">
+            <form id="form-cari" action="<?php echo site_url('/dirjen/frontdesk/index');?>" method="post">
+                <p><input type="text" size="60" placeholder="Pencarian" name="keyword" value="<?php echo $isian_form;?>"/>
+                    &nbsp;<input type="submit" value="cari" class="button blue-pill"/>
+                    <a href="<?php echo site_url('/dirjen/frontdesk/index');?>" class="button gray-pill">Reset</a>
+                </p>
+            </form>
+        </div>
+
+
+        <table class="table">
+            <thead>
+            <tr>
+                <th class="no">No</th>
+                <th class="short">Tanggal Pengajuan</th>
+                <th class="no">Proses (Hari)</th>
+                <th>Nama K/L</th>
+                <th>Nama Eselon</th>
+                <th>&nbsp;</th>
+            </tr>
+            </thead>
+            <tfoot>
+            <tr>
+                <td colspan="6">&nbsp;</td>
+            </tr>
+            </tfoot>
+            <tbody>
+                <?php $i = 1 ?>
+                <?php foreach ($result->result() as $value): ?>
+
+                <?php
+                $date1 = new DateTime(date('Y-m-d H:i:s'));
+                $date2 = new DateTime($value->tanggal);
+                //                            $day = $date1->diff($date2);
+                $day = date_diff($date1, $date2);
+
+                $hari = $day->days;
+
+                //TODO: Algoritmanya kemungkinan salah
+                $hari_kerja = (int)($day->days / 7) * 2;
+                $hari_kerja = $hari - $hari_kerja;
+
+                $sql = "select * from tb_calendar WHERE holiday BETWEEN '{$value->tanggal}' AND NOW()";
+                $hari_kerja = $hari_kerja - $this->db->query($sql)->num_rows();
+
+                if ($day->days > 4) {
+//                    echo '<span style="font-weight: bold; font-size: 13px; color: white; background: tomato; padding: 0 4px;">' . $hari_kerja . '</span>';
+                } elseif ($day->days == 4) {
+//                    echo '<span style="font-weight: bold; font-size: 13px; color: yellow; background: black; padding: 0 4px;">' . $hari_kerja . '</span>';
+                } else {
+//                    echo '<span style="">' . $day->days . '</span>';
+                }
+                ?>
+
+            <tr style="<?php echo ($day->days > 4) ? "background: pink; color: white;" : '' ?>">
+                <td><?php echo $i++ ?></td>
+                <td>
+                    <?php echo table_tanggal($value->tanggal) ?>
+                </td>
+                <td>
+                    <?php echo $hari_kerja ?>
+                </td>
+                <td><?php echo $value->nama_kementrian ?></td>
+                <td><?php echo $value->nama_unit ?></td>
+                <td class="action">
+					<?php
+						$disabled =  $onclick = '';
+						$style_button = 'gray-pill';
+						if($value->is_active == 2){
+							$disabled = 'disabled';
+							$style_button = 'blue-pill';
+						}
+					?>
+					<a class="button <?php echo $style_button;?>" href="<?php echo site_url('/dirjen/diterima/' . $value->no_tiket_frontdesk) ?>">Diterima</a>                  
+                    <a class="button " href="<?php echo site_url('/dirjen/reject/' . $value->no_tiket_frontdesk) ?>">Dikembalikan</a>
+                    <a class="button <?php echo $style_button;?>" href="<?php echo site_url('/dirjen/accept/' . $value->no_tiket_frontdesk) ?>">Ditetapkan</a>
+                </td>
+            </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+    </div>
+    <div class="pagination"><?php echo ($pageLink) ? 'Halaman ' . $pageLink : '';?></div>
+    <br/>
+    </div>
+    <?php else: ?>
+
+Tidak ada dokumen
+
+<?php endif ?>
+</div>
+
+<script type="text/javascript">
+    $(function () {
+        $('.bla').click(function () {
+            $(this).next().removeClass('gray-pill').addClass('blue-pill').removeAttr('disabled');
+            link = $(this).next().attr('link');
+            $(this).next().attr('onclick', 'window.location.href="' + link + '"');
+            console.log(link);
+            return false;
+        })
+    })
+</script>
---
+++ application/views/dirjen/reject.php
@@ -0,0 +1,45 @@
+<div class="content">
+    <h1>Form Pengembalian Dokumen</h1>
+
+    <div>
+        <p>
+            <label style="width: 120px; display: inline-block;">No Tiket</label>
+            <span>#<?php echo sprintf('%05d', $this->uri->segment(4)) ?></span>
+        </p>
+		
+		<p>
+            <label style="width: 120px; display: inline-block;">Kode - Nama K/L</label>
+            <span><?php echo $data->id_kementrian.' - '.$data->nama_kementrian; ?></span>
+        </p>
+		
+		<p>
+            <label style="width: 120px; display: inline-block;">Kode - Nama Eselon</label>
+            <span><?php echo $data->id_unit.' - '.$data->nama_unit; ?></span>
+        </p>
+		
+        <p>
+            <label style="width: 120px; display: inline-block;">Kode Satker</label>
+            <span><?php echo $data->id_satker ?></span>
+        </p>
+
+        <p>
+            <label style="width: 120px; display: inline-block;">Nama Satker</label>
+            <span><?php echo $data->nama_satker ?></span>
+        </p>
+    </div>
+
+    <form method="post" action="<?php echo site_url('/dirjen/reject/' . $this->uri->segment(3)) ?>">
+        <input type="hidden" name="no_tiket_frontdesk" value="<?php echo $data->no_tiket_frontdesk; ?>"/>
+        <input type="hidden" name="id_user" value="<?php echo $data->id_satker ?>"/>
+
+        <p>
+            <label>Catatan Pengembalian Dokumen<br/>
+                <textarea name="catatan" rows="10" cols="170"></textarea>
+            </label>
+        </p>
+
+        <p>
+            <input type="submit" value="Kirim" class="button blue-pill"/>
+        </p>
+    </form>
+</div>
