7446918127cf865482990c8d188cf69a1d35b8aa
mul14 <mul14.net@gmail.com>
2012-01-13 15:01:07 +0700

    Tambah default title

--- application/views/new-template.php
+++ application/views/new-template.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-    <title><?php echo $title ?></title>
+    <title>Sistem Informasi Pusat Layanan DJA - <?php echo $title ?></title>
     <meta name="description" content="">
     <meta name="author" content="">
 
