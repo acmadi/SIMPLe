989acc0c77ea2f64095cd81a62125867348dedc7
Mulia A. Nasution <mul14.net@gmail.com>
2012-01-16 04:14:39 +0700

    Direktur - Helpdesk

--- application/controllers/direktur/helpdesk.php
+++ application/controllers/direktur/helpdesk.php
@@ -10,23 +10,80 @@ class Helpdesk extends CI_Controller
 
     function index()
     {
+        $this->search(NULL);
+    }
+
+    function search($keyword = NULL)
+    {
+        $keyword = ($this->input->post('keyword'))
+                ? $this->input->post('keyword')
+                : NULL;
+        $keyword = ($keyword != NULL)
+                ? "AND tb_satker.id_satker LIKE '%$keyword%'"
+                : '';
         $sql = "SELECT * FROM tb_tiket_helpdesk JOIN tb_satker
                                 ON tb_tiket_helpdesk.id_satker = tb_satker.id_satker
-                                WHERE status = 'open'";
+                                WHERE status = 'open' AND
+                                lavel = 5 $keyword";
         $data['antrian'] = $this->db->query($sql);
 
         $data['title'] = 'Konsultasi Help Desk';
-        $data['content'] = 'pelaksana/helpDesk';
+        $data['content'] = 'direktur/helpdesk';
         $this->load->view('new-template', $data);
     }
 
     function view($id)
     {
         $data['title'] = 'Cek Tiket';
-        $data['content'] = 'pelaksana/helpdesk_view';
+        $data['content'] = 'direktur/helpdesk_view';
         $data['antrian'] = $this->mhelpdesk->get_by_id($id);
 
         $this->load->view('new-template', $data);
+    }
+
+    // TODO: Untuk konsistensi, mungkin sebaiknya eskalasi dan jawab dipisah. Atau ganti nama eskalasi() dengan nama tertentu
+    function eskalasi()
+    {
+        if ($this->input->post('submit') == 'Eskalasi') {
+
+            $this->db->update('tb_tiket_helpdesk', array(
+                'lavel' => 6,
+            ), array(
+                'no_tiket_helpdesk' => $this->input->post('no_tiket_helpdesk')
+            ));
+
+            $this->_success(site_url('direktur/helpdesk'), 'Pertanyaan berhasil dieskalasi ke Dirjen', 5);
+
+        }
+
+        if ($this->input->post('submit') == 'Jawab') {
+
+            $this->db->update('', array(
+                'jawaban' => $this->input->post('jawaban'),
+                'nama_narasumber' => $this->input->post('nama_narasumber'),
+                'jabatan_narasumber' => $this->input->post('jabatan')
+            ), array(
+                'no_tiket_helpdesk' => $this->input->post('no_tiket_helpdesk')
+            ));
+
+            $this->db->insert('tb_knowledge_base', array(
+                'judul' => $this->input->post('pertanyaan'),
+                'jawaban' => $this->input->post('jawaban'),
+                'nama_narasumber' => $this->input->post('nama_narasumber'),
+                'jabatan_narasumber' => $this->input->post('jabatan')
+            ));
+
+            $this->_success(site_url('direktur/helpdesk'), 'Pertanyaan telah berhasil dijawab dan telah dimasukkan ke knowledge base', 5);
+        }
+    }
+
+    private function _success($url, $message, $time)
+    {
+        $data['url'] = $url;
+        $data['message'] = $message;
+        $data['time'] = $time;
+
+        $this->load->view('helpdesk/success', $data);
     }
 
 }
