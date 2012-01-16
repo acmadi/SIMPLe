1c5d5fa6664cd2fcc919f39a8d186d3c0df4b01e
Fadhli Daswir <ar_raudy@yahoo.com>
2012-01-17 00:35:19 +0700

    tambah tabel tb_online_users baru, ganti proses penyimpanan session ke dalam file

--- application/config/config.php
+++ application/config/config.php
@@ -247,9 +247,9 @@ $config['encryption_key'] = 'komuridja';
 */
 $config['sess_cookie_name']		= 'ci_session';
 $config['sess_expiration']		= 7200;
-$config['sess_expire_on_close']	= FALSE;
+$config['sess_expire_on_close']	= TRUE;
 $config['sess_encrypt_cookie']	= FALSE;
-$config['sess_use_database']	= TRUE;
+$config['sess_use_database']	= FALSE;
 $config['sess_table_name']		= 'ci_sessions';
 $config['sess_match_ip']		= TRUE;
 $config['sess_match_useragent']	= TRUE;
