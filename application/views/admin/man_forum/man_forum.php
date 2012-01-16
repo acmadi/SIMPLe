f6860f932eb22d1f17471d432be8412e506d9f5f
Akhyar <akhyrul@gmail.com>
2012-01-16 11:10:41 +0700

    tuker posisi

--- application/views/admin/man_forum/man_forum.php
+++ application/views/admin/man_forum/man_forum.php
@@ -1,7 +1,7 @@
 <ul id="nav">
     <li><a href="#tab1" class="active">Forum</a></li>
-    <li><a href="#tab2">Kategori Forum</a></li>
     <li><a href="#tab3">Tambah Forum</a></li>
+    <li><a href="#tab2">Kategori Forum</a></li>
 </ul>
 <div class="clear"></div>
 
@@ -78,6 +78,54 @@
         </div>
 
 
+
+
+        <div style="display: none;" id="tab3" class="tab_konten">
+
+
+            <div class="table">
+                <form action="<?php echo site_url('/admin/man_forum/add_forum'); ?>" method="post" enctype="multipart/form-data" accept-charset="utf-8"
+                      style="border: 1px solid #999; padding: 13px 30px 13px 13px; margin:5px 0px 0px 20px; font-size:12px">
+                    <table>
+                        <tr>
+                            <td width="100px">Kategori Forum</td>
+                            <td>:</td>
+                            <td>
+                                <select name="id_kat_forum">
+                                    <?php foreach ($categories->result() as $category): ?>
+                                    <option value="<?php echo $category->id_kat_forum ?>"><?php echo $category->kat_forum ?></option>
+                                    <?php endforeach ?>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Judul</td>
+                            <td>:</td>
+                            <td><input type="text" name="judul_forum" size="65"/></td>
+                        </tr>
+                        <tr>
+                            <td valign="top">Isi</td>
+                            <td valign="top">:</td>
+                            <td><textarea name="isi_forum" cols="58" rows="15"></textarea>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td valign="top">Lampiran</td>
+                            <td valign="top">:</td>
+                            <td><input type="file" name="lampiran"></td>
+                        </tr>
+                    </table>
+                    
+                    <input class="button blue-pill" type="submit" value="Tambah"/>
+                    <input type="button" value="reset" class="button gray-pill"/>
+                                        
+                </form>
+            </div>
+        </div>
+
+
+
+
         <div style="display: none;" id="tab2" class="tab_konten">
 
             <form action="<?php echo site_url('/admin/man_forum/add_category') ?>" method="post"
@@ -127,53 +175,5 @@
                 </div>
             </div>
         </div>
-
-
-        <div style="display: none;" id="tab3" class="tab_konten">
-
-
-            <div class="table">
-                <form action="<?php echo site_url('/admin/man_forum/add_forum'); ?>" method="post" enctype="multipart/form-data" accept-charset="utf-8"
-                      style="border: 1px solid #999; padding: 13px 30px 13px 13px; margin:5px 0px 0px 20px; font-size:12px">
-                    <table>
-                        <tr>
-                            <td width="100px">Kategori Forum</td>
-                            <td>:</td>
-                            <td>
-                                <select name="id_kat_forum">
-                                    <?php foreach ($categories->result() as $category): ?>
-                                    <option value="<?php echo $category->id_kat_forum ?>"><?php echo $category->kat_forum ?></option>
-                                    <?php endforeach ?>
-                                </select>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Judul</td>
-                            <td>:</td>
-                            <td><input type="text" name="judul_forum" size="65"/></td>
-                        </tr>
-                        <tr>
-                            <td valign="top">Isi</td>
-                            <td valign="top">:</td>
-                            <td><textarea name="isi_forum" cols="58" rows="15"></textarea>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td valign="top">Lampiran</td>
-                            <td valign="top">:</td>
-                            <td><input type="file" name="lampiran"></td>
-                        </tr>
-                    </table>
-					
-					<input class="button blue-pill" type="submit" value="Tambah"/>
-					<input type="button" value="reset" class="button gray-pill"/>
-                                        
-                </form>
-            </div>
-        </div>
-
-
-
-
 
         <script type="text/javascript" src="<?php echo base_url(); ?>js/fungsi.js"></script>
