3706ebe9609557f3b003b78e73e857912f5c8e27
Akhyar <akhyrul@gmail.com>
2012-01-16 21:12:46 +0700

    supervisor: lanjutan report CS helpdesk

--- application/views/supervisor/report_helpdesk.php
+++ application/views/supervisor/report_helpdesk.php
@@ -1,28 +1,74 @@
+<script>
+function knowledge(id){
+	// alert(id);
+	var knowledge_base_id = id;
+    var url = '<?php echo site_url('helpdesk/knowledge_base/get_by_id/') ?>/' + knowledge_base_id;
+
+    $.get(url, function (response) {
+        $('#jawaban').html(response).dialog('open');
+    });
+}
+$(document).ready(function(){
+
+	$('#jawaban').dialog('destroy');
+
+    $('#jawaban').dialog({
+        autoOpen:false,
+        title:'Referensi Jawaban',
+        modal:true,
+        resizable:false,
+        draggable:false,
+        width:700,
+        height:400,
+        dialogClass:'centered-dialog',
+
+    });
+
+});
+</script>
 <div class="content">
     <h1>Pertanyaan yang Terjawab oleh CS Helpdesk</h1>
 
-    <table>
+    <table class="table">
+    <thead>
     <tr>
-    	<th>Ditanyakan</th>
-    	<th>Terjawab</th>
-    	<th>ID / Nama CS</th>
-    	<th>Pertanyaan</th>
-    	<th>Jawaban</th>
+    	<th class="no">No</th>
+    	<th class="no">Terjawab</th>
+    	<th class="no">ID / Nama CS</th>
+    	<th class="no">Pertanyaan</th>
+    	<th class="no">Deskripsi</th>
+    	<th class="no">Jawaban</th>
     </tr>
-    <?php foreach($tiket_helpdesk as $tiket) : ?>
+    </thead>
+    <tbody>
+    <?php $i=1; foreach($tiket_helpdesk as $tiket) : ?>
   	<tr>
-  		<td><?php echo $tiket->tanggal ?></td>
-  		<td><?php echo $tiket->timestamp ?></td>
-  		<td><?php echo $tiket->username . ' / ' . $tiket->nama ?></td>
-  		<td><?php echo $tiket->pertanyaan ?></td>
-  		<td><?php echo 'jawaban' ?></td>
+  		<td class="no"><?php echo $i++ ?></td>
+  		<td class="no"><?php echo $tiket->timestamp ?></td>
+  		<td class="no"><?php echo $tiket->username . ' / ' . $tiket->nama ?></td>
+  		<td class="no"><?php echo $tiket->pertanyaan ?></td>
+  		<td class="no"><?php echo $tiket->description ?></td>
+
+  		<td class="no"><?php 
+  						echo ($tiket->id_knowledge_base != NULL) ?
+  						form_button('show', 'Lihat', 
+  						'class="button" 
+  						 onclick="return knowledge('.$tiket->id_knowledge_base.')"') . ' ' .
+  					    'Knowledge base #' . $tiket->id_knowledge_base : 
+  					    ' - ';
+  					    ?></td>
   	</tr>
     <?php endforeach ?>
+    </tbody>
+
     </table>
     <?php 
+    if(ENVIRONMENT == 'development'):
     echo '<strong>Dump Data, development only</strong>';
     echo '<pre>'; 
     var_dump($tiket_helpdesk); 
     echo '</pre>';
+    endif;
     ?>
 </div>
+<div id="jawaban" style="display: none;"></div>
