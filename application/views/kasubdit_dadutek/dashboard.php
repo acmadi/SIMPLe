18c2ee3767802e970f4fc02af43fba6fd9bdef6f
Febri Valentino <febrivalentino@rocketmail.com>
2012-01-20 08:34:57 +0700

    cuman ganti nama Dashboadnya

--- application/views/kasubdit_dadutek/dashboard.php
+++ application/views/kasubdit_dadutek/dashboard.php
@@ -5,7 +5,7 @@
 </style>
 
 <div class="content">
-    <h1>Kasubdit Anggaran Dashboard</h1>
+    <h1>Kasubdit DADUTEK Dashboard</h1>
 
     <fieldset class="grid_5">
         <legend>Report Front Desk</legend>
