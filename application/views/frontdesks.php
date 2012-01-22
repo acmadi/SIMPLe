fd76894b27e16660026c2a40b5d0f49421366045
Mulia A. Nasution <mul14.net@gmail.com>
2012-01-22 17:25:42 +0700

    Rapikan, ganti hari_kerja dengan helper.

--- application/views/frontdesks.php
+++ application/views/frontdesks.php
@@ -7,8 +7,8 @@
                 <th class="no">No</th>
                 <th class="short">Tanggal Pengajuan</th>
                 <th class="no">Proses (Hari)</th>
-                <th>Nama K/L</th>
-                <th>Nama Eselon</th>
+                <th>Kementrian</th>
+                <th>Eselon</th>
                 <th>&nbsp;</th>
             </tr>
             </thead>
@@ -21,40 +21,16 @@
                 <?php $i = 1 ?>
                 <?php foreach ($result->result() as $value): ?>
 
-                <?php
-                $date1 = new DateTime(date('Y-m-d H:i:s'));
-                $date2 = new DateTime($value->tanggal);
-                //                            $day = $date1->diff($date2);
-                $day = date_diff($date1, $date2);
-
-                $hari = $day->days;
-
-                //TODO: Algoritmanya kemungkinan salah
-                $hari_kerja = (int)($day->days / 7) * 2;
-                $hari_kerja = $hari - $hari_kerja;
-
-                $sql = "select * from tb_calendar WHERE holiday BETWEEN '{$value->tanggal}' AND NOW()";
-                $hari_kerja = $hari_kerja - $this->db->query($sql)->num_rows();
-
-                if ($day->days > 4) {
-//                    echo '<span style="font-weight: bold; font-size: 13px; color: white; background: tomato; padding: 0 4px;">' . $hari_kerja . '</span>';
-                } elseif ($day->days == 4) {
-//                    echo '<span style="font-weight: bold; font-size: 13px; color: yellow; background: black; padding: 0 4px;">' . $hari_kerja . '</span>';
-                } else {
-//                    echo '<span style="">' . $day->days . '</span>';
-                }
-                ?>
-
-            <tr style="<?php echo ($day->days > 4) ? "background: pink; color: white;" : '' ?>">
+            <tr <?php echo (hari_kerja($value->tanggal > 5) ? 'class="red-row"' : '') ?>>
                 <td><?php echo $i++ ?></td>
                 <td>
                     <?php echo table_tanggal($value->tanggal) ?>
                 </td>
                 <td>
-                    <?php echo $hari_kerja ?>
+                    <?php echo hari_kerja($value->tanggal) ?>
                 </td>
-                <td><?php echo $value->nama_kementrian ?></td>
-                <td><?php echo $value->nama_unit ?></td>
+                <td><strong><?php echo $value->id_kementrian ?></strong> - <?php echo $value->nama_kementrian ?></td>
+                <td><strong><?php echo $value->id_unit ?></strong> - <?php echo $value->nama_unit ?></td>
                 <td class="action">
 					<?php
 						$disabled =  $onclick = '';
@@ -63,54 +39,54 @@
 							$disabled = 'disabled';
 							$style_button = 'blue-pill';
 						}
-						
+
 						$level_selected = $this->session->userdata('lavel');
 						switch($level_selected){
 							case '3':
 								$jml_sel = 2;
 								break;
-							
+
 							case '6':
 								$jml_sel = 3;
 								break;
-							
+
 							case '7':
 								$jml_sel = 2;
 								break;
-								
+
 							default:
 								$jml_sel = 1;
 								break;
 						}
 					?>
-					
+
 					<a class="button <?php echo $style_button;?>" href="<?php echo site_url('/frontdesks/diterima/' . $value->no_tiket_frontdesk) ?>">Diterima</a>
-                    
+
 					<?php if($level_selected != '7'): ?>
-					<input type="button" class="button <?php echo $style_button;?>" onclick="window.location.href='<?php echo site_url('/frontdesks/diteruskan/' . $value->no_tiket_frontdesk); ?>'" 
+					<input type="button" class="button <?php echo $style_button;?>" onclick="window.location.href='<?php echo site_url('/frontdesks/diteruskan/' . $value->no_tiket_frontdesk); ?>'"
 					<?php echo $disabled;?> value="Diteruskan"/>
                     <?php endif; ?>
-					
-					
+
+
 					<?php if($level_selected == '3'): ?>
-						<input type="button" class="button <?php echo $style_button;?>" onclick="window.location.href='<?php echo site_url('/frontdesks/reject/' . $value->no_tiket_frontdesk); ?>'" 
+						<input type="button" class="button <?php echo $style_button;?>" onclick="window.location.href='<?php echo site_url('/frontdesks/reject/' . $value->no_tiket_frontdesk); ?>'"
 						<?php echo $disabled;?> value="Dikembalikan"/>
                     <?php endif; ?>
-					
+
 					<?php if(($level_selected == '7') or ($level_selected == '6')): ?>
-					<input type="button" class="button <?php echo $style_button;?>" onclick="window.location.href='<?php echo site_url('/frontdesks/accept/' . $value->no_tiket_frontdesk); ?>'" 
+					<input type="button" class="button <?php echo $style_button;?>" onclick="window.location.href='<?php echo site_url('/frontdesks/accept/' . $value->no_tiket_frontdesk); ?>'"
 					<?php echo $disabled;?> value="Ditetapkan"/>
-					<input type="button" class="button <?php echo $style_button;?>" onclick="window.location.href='<?php echo site_url('/frontdesks/reject/' . $value->no_tiket_frontdesk); ?>'" 
+					<input type="button" class="button <?php echo $style_button;?>" onclick="window.location.href='<?php echo site_url('/frontdesks/reject/' . $value->no_tiket_frontdesk); ?>'"
 					<?php echo $disabled;?> value="Ditolak"/>
                     <?php endif; ?>
-					
+
                 </td>
             </tr>
                 <?php endforeach ?>
             </tbody>
         </table>
- 
-    
+
+
     <br/>
     </div>
     <?php else: ?>
@@ -119,14 +95,3 @@
     </div>
     <?php endif ?>
 </div>
-
-<script type="text/javascript">
-    $(function () {
-        $('.bla').click(function () {
-			<?php for($i = 0 ; $i < $jml_sel ; $i++){?>
-				$(this).next().removeClass('gray-pill').addClass('blue-pill').removeAttr('disabled');
-			<?php }?>
-            return false;
-        })
-    })
-</script>
