4bb22b8d6d62707255f64948cf2b329d771c0726
Fadhli Daswir <ar_raudy@yahoo.com>
2012-01-22 15:25:18 +0700

    konversi kode cari waktu kerja ke SQL, tahap penggabungan halaman dashboard beberapa user level

---
+++ application/controllers/dashboards.php
@@ -0,0 +1,58 @@
+<?php
+class Dashboards extends CI_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('mfrontdesk');
+        $this->load->model('mhelpdesk');
+    }
+
+    function index()
+    {
+		//status, lavel, is_active
+        $data['helpdesk_total'] = $this->mhelpdesk->count_all_tiket('open', 3);
+        $data['helpdesk_total_cs'] = $this->mhelpdesk->count_all_tiket('close', 1);
+        $data['helpdesk_total_supervisor'] = $this->mhelpdesk->count_all_tiket('close', 2);
+        $data['helpdesk_total_pelaksana'] = $this->mhelpdesk->count_all_tiket('close', 3);
+
+
+        $data['frontdesk_total'] 		= $this->mfrontdesk->count_all_tiket_frontdesk('open',$this->session->userdata('lavel')); 
+		
+        $data['total_tiket_diterima_cs'] = $this->mfrontdesk->count_all_tiket_frontdesk('open',1,1); 
+        $data['total_tiket_diteruskan_cs'] = $this->mfrontdesk->count_all_tiket_frontdesk('open',3,2);
+		
+        $data['total_tiket_diterima_pelaksana'] = $this->mfrontdesk->count_all_tiket_frontdesk('open',3,1);
+        $data['total_tiket_diteruskan_pelaksana'] = $this->mfrontdesk->count_all_tiket_frontdesk('open',4,2);
+		
+		$data['total_tiket_diterima_kasubdit'] = $this->mfrontdesk->count_all_tiket_frontdesk('open',4,1);
+        $data['total_tiket_diteruskan_kasubdit'] = $this->mfrontdesk->count_all_tiket_frontdesk('open',5,2);
+		
+		$data['total_tiket_diterima_dadutek'] = $this->mfrontdesk->count_all_tiket_frontdesk('open',5,1);
+        $data['total_tiket_diteruskan_dadutek'] = $this->mfrontdesk->count_all_tiket_frontdesk('open',6,2);
+		
+		$data['total_tiket_diterima_direktur'] = $this->mfrontdesk->count_all_tiket_frontdesk('open',6,1);
+        $data['total_tiket_diteruskan_direktur'] = $this->mfrontdesk->count_all_tiket_frontdesk('open',7,2);
+		
+        $data['total_tiket_dtetap_direktur'] = $this->mfrontdesk->count_all_tiket_frontdesk('close',6,'','disahkan');
+        $data['total_tiket_dtolak_direktur'] = $this->mfrontdesk->count_all_tiket_frontdesk('close',6,'','ditolak');
+		
+		$data['total_tiket_open_cs'] = $this->mfrontdesk->count_all_tiket_frontdesk('open',1);
+		$data['total_tiket_open_pelaksana'] = $this->mfrontdesk->count_all_tiket_frontdesk('open',3);
+		$data['total_tiket_open_kasubdit'] = $this->mfrontdesk->count_all_tiket_frontdesk('open',4);
+		$data['total_tiket_open_dadutek'] = $this->mfrontdesk->count_all_tiket_frontdesk('open',5);
+		$data['total_tiket_open_direktur'] = $this->mfrontdesk->count_all_tiket_frontdesk('open',6);
+
+        $merah = $this->db->query('SELECT * from tb_tiket_frontdesk WHERE (DATEDIFF(NOW(), tanggal) > 5)');
+        if ($merah->num_rows() > 0) {
+            $data['merah'] = true;
+        }
+
+        $data['title'] = 'Dashboard';
+        $data['content'] = 'dashboards';
+        $this->load->view('new-template', $data);
+    }
+}
+
+?>
---
+++ application/views/dashboards.php
@@ -0,0 +1,101 @@
+<style>
+    .message_box {
+        text-align: center;
+    }
+</style>
+
+<div class="content">
+    <h1>Kasie & Pelaksana Dashboard</h1>
+
+    <fieldset class="grid_5">
+        <legend>Report Front Desk</legend>
+        <div class="message_box">
+            <?php
+            if (isset($merah)) {
+                $merah = 'red';
+            } else {
+                $merah = 'green';
+            }
+            ?>
+            <a class="button <?php echo $merah ?>" href="<?php echo site_url('/frontdesks') ?>"><?php echo $frontdesk_total ?> Tiket</a>
+        </div>
+        <table class="table">
+            <tr>
+                <td>&nbsp;</td>
+                <td class="head">Diterima</td>
+                <td class="head">Diteruskan</td>
+                <td class="head">Open</td>
+                <td class="head">Ditetapkan</td>
+                <td class="head">Ditolak</td>
+            </tr>
+            <tr>
+                <td class="head">CS</td>
+                <td><span class="message_box glow_green"><?php echo $total_tiket_diterima_cs ?></span></td>
+                <td><span class="message_box glow_green"><?php echo $total_tiket_diteruskan_cs?></span></td>
+                <td><span class="message_box glow_green"><?php echo $total_tiket_open_cs ?></span></td>
+                <td><span class="message_box glow_green">0</span></td>
+                <td><span class="message_box glow_green">0</span></td>
+            </tr>
+            <tr>
+                <td class="head">Kasi & Pelaksana</td>
+                <td><span class="message_box glow_green"><?php echo $total_tiket_diterima_pelaksana ?></span></td>
+                <td><span class="message_box glow_green"><?php echo $total_tiket_diteruskan_pelaksana ?></span></td>
+                <td><span class="message_box glow_green"><?php echo $total_tiket_open_pelaksana ?></span></td>
+                <td><span class="message_box glow_green">0</span></td>
+                <td><span class="message_box glow_green">0</span></td>
+            </tr>
+			<tr>
+                <td class="head">Kasubdit Anggaran</td>
+                <td><span class="message_box glow_green"><?php echo $total_tiket_diterima_kasubdit ?></span></td>
+                <td><span class="message_box glow_green"><?php echo $total_tiket_diteruskan_kasubdit ?></span></td>
+                <td><span class="message_box glow_green"><?php echo $total_tiket_open_kasubdit ?></span></td>
+                <td><span class="message_box glow_green">0</span></td>
+                <td><span class="message_box glow_green">0</span></td>
+            </tr>
+			<tr>
+                <td class="head">Kasubdit Dadutek</td>
+                <td><span class="message_box glow_green"><?php echo $total_tiket_diterima_dadutek ?></span></td>
+                <td><span class="message_box glow_green"><?php echo $total_tiket_diteruskan_dadutek ?></span></td>
+                <td><span class="message_box glow_green"><?php echo $total_tiket_open_dadutek ?></span></td>
+                <td><span class="message_box glow_green">0</span></td>
+                <td><span class="message_box glow_green">0</span></td>
+            </tr>
+			<tr>
+                <td class="head">Direktur</td>
+                <td><span class="message_box glow_green"><?php echo $total_tiket_diterima_direktur ?></span></td>
+                <td><span class="message_box glow_green"><?php echo $total_tiket_diteruskan_direktur ?></span></td>
+                <td><span class="message_box glow_green"><?php echo $total_tiket_open_direktur ?></span></td>
+                <td><span class="message_box glow_green"><?php echo $total_tiket_dtetap_direktur ?></span></td>
+                <td><span class="message_box glow_green"><?php echo $total_tiket_dtolak_direktur ?></span></td>
+            </tr>
+        </table>
+
+        <div>
+            <span style="background: red; padding: 0 8px;"></span>&nbsp;
+            Warna merah menunjukkan proses penyelesaian telah melebihi 5 hari kerja
+        </div>
+    </fieldset>
+
+    <fieldset class="grid_5">
+        <legend>Report Help Desk</legend>
+        <div class="message_box">
+            <a class="button green" href="<?php echo site_url('/pelaksana/helpdesk') ?>"><?php echo $helpdesk_total ?> Pertanyaan</a>
+        </div>
+        <table class="table">
+            <caption>Jumlah pertanyaan yang langsung diselesaikan oleh:</caption>
+            <tr>
+                <td class="head"><span class="label">CS</span></td>
+                <td><span class="message_box glow_green number"><?php echo $helpdesk_total_cs ?></span></td>
+            </tr>
+            <tr>
+                <td class="head"><span class="label">Supervisor</span></td>
+                <td><span class="message_box glow_green number"><?php echo $helpdesk_total_supervisor ?></span></td>
+            </tr>
+            <tr>
+                <td class="head"><span class="label">Kasi & Pelaksana</span></td>
+                <td><span class="message_box glow_green number"><?php echo $helpdesk_total_pelaksana ?></span></td>
+            </tr>
+        </table>
+    </fieldset>
+
+</div>
