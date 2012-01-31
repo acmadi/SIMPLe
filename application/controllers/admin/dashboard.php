d0f90c163bb9e49591b30d33e77a62fc7ed396ee
fadhliku <ar_raudy@yahoo.com>
2012-01-31 16:19:52 +0700

    dashboard admin

--- application/controllers/admin/dashboard.php
+++ application/controllers/admin/dashboard.php
@@ -16,65 +16,68 @@ class Dashboard extends CI_Controller
           if ($this->session->userdata('login') == TRUE)
           {*/
         // Standard inclusions
-        include("pChart/pData.class");
-        include("pChart/pChart.class");
+		$result_statistik = $this->mdashboard->get_info_statistik();
+		
+		if($result_statistik):
+			include("pChart/pData.class");
+			include("pChart/pChart.class");
 
-        $result_statistik = $this->mdashboard->get_info_statistik();
+			
 
-        $tmp = array();
-        $tmp2 = array();
+			$tmp = array();
+			$tmp2 = array();
 
-        foreach ($result_statistik['tahun'] as $th):
-            if (isset($result_statistik['front'][$th])) {
-                $tmp[] = $result_statistik['front'][$th];
-            } else {
-                $tmp[] = 0;
-            }
+			foreach ($result_statistik['tahun'] as $th):
+				if (isset($result_statistik['front'][$th])) {
+					$tmp[] = $result_statistik['front'][$th];
+				} else {
+					$tmp[] = 0;
+				}
 
-            if (isset($result_statistik['help'][$th])) {
-                $tmp2[] = $result_statistik['help'][$th];
-            } else {
-                $tmp2[] = 0;
-            }
-        endforeach;
+				if (isset($result_statistik['help'][$th])) {
+					$tmp2[] = $result_statistik['help'][$th];
+				} else {
+					$tmp2[] = 0;
+				}
+			endforeach;
 
 
-        // Dataset definition
-        $DataSet = new pData;
-        $DataSet->AddPoint($tmp, "Serie1");
-        $DataSet->AddPoint($tmp2, "Serie2");
+			// Dataset definition
+			$DataSet = new pData;
+			$DataSet->AddPoint($tmp, "Serie1");
+			$DataSet->AddPoint($tmp2, "Serie2");
 
-        $DataSet->AddAllSeries();
-        $DataSet->AddPoint($result_statistik['tahun'], "serie3");
-        $DataSet->SetAbsciseLabelSerie("serie3");
+			$DataSet->AddAllSeries();
+			$DataSet->AddPoint($result_statistik['tahun'], "serie3");
+			$DataSet->SetAbsciseLabelSerie("serie3");
 
-        $DataSet->SetSerieName("Front desk (" . $result_statistik['jml_1'] . " Org)", "Serie1");
-        $DataSet->SetSerieName("Help desk (" . $result_statistik['jml_2'] . " Org)", "Serie2");
+			$DataSet->SetSerieName("Front desk (" . $result_statistik['jml_1'] . " Org)", "Serie1");
+			$DataSet->SetSerieName("Help desk (" . $result_statistik['jml_2'] . " Org)", "Serie2");
 
-        // Initialise the graph
-        $Test = new pChart(500, 230);
-        $Test->setFontProperties("Fonts/tahoma.ttf", 8);
-        $Test->setGraphArea(50, 30, 330, 200);
-        $Test->drawFilledRoundedRectangle(7, 7, 493, 223, 5, 240, 240, 240);
-        $Test->drawRoundedRectangle(5, 5, 495, 225, 5, 230, 230, 230);
-        $Test->drawGraphArea(255, 255, 255, TRUE);
-        $Test->drawScale($DataSet->GetData(), $DataSet->GetDataDescription(), SCALE_NORMAL, 150, 150, 150, TRUE, 0, 2, TRUE);
-        $Test->drawGrid(4, TRUE, 230, 230, 230, 50);
+			// Initialise the graph
+			$Test = new pChart(500, 230);
+			$Test->setFontProperties("Fonts/tahoma.ttf", 8);
+			$Test->setGraphArea(50, 30, 330, 200);
+			$Test->drawFilledRoundedRectangle(7, 7, 493, 223, 5, 240, 240, 240);
+			$Test->drawRoundedRectangle(5, 5, 495, 225, 5, 230, 230, 230);
+			$Test->drawGraphArea(255, 255, 255, TRUE);
+			$Test->drawScale($DataSet->GetData(), $DataSet->GetDataDescription(), SCALE_NORMAL, 150, 150, 150, TRUE, 0, 2, TRUE);
+			$Test->drawGrid(4, TRUE, 230, 230, 230, 50);
 
-        // Draw the 0 line
-        $Test->setFontProperties("Fonts/tahoma.ttf", 6);
-        $Test->drawTreshold(0, 143, 55, 72, TRUE, TRUE);
+			// Draw the 0 line
+			$Test->setFontProperties("Fonts/tahoma.ttf", 6);
+			$Test->drawTreshold(0, 143, 55, 72, TRUE, TRUE);
 
-        // Draw the bar graph
-        $Test->drawBarGraph($DataSet->GetData(), $DataSet->GetDataDescription(), TRUE);
+			// Draw the bar graph
+			$Test->drawBarGraph($DataSet->GetData(), $DataSet->GetDataDescription(), TRUE);
 
-        // Finish the graph
-        $Test->setFontProperties("Fonts/tahoma.ttf", 8);
-        $Test->drawLegend(350, 100, $DataSet->GetDataDescription(), 255, 255, 255);
-        $Test->setFontProperties("Fonts/tahoma.ttf", 10);
-        $Test->drawTitle(50, 22, "Statistik Pengunjung", 50, 50, 50, 385);
-        $Test->Render("images/dashboard_bar_admin.png");
-
+			// Finish the graph
+			$Test->setFontProperties("Fonts/tahoma.ttf", 8);
+			$Test->drawLegend(350, 100, $DataSet->GetDataDescription(), 255, 255, 255);
+			$Test->setFontProperties("Fonts/tahoma.ttf", 10);
+			$Test->drawTitle(50, 22, "Statistik Pengunjung", 50, 50, 50, 385);
+			$Test->Render("images/dashboard_bar_admin.png");
+		endif;
 
         $data['title'] = 'Dashboard';
         $data['content'] = 'admin/dashboard';
