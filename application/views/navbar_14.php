49333a3762cda418d5e2b6f7bfcfb227f0ae8312
Mulia A. Nasution <mul14.net@gmail.com>
2012-02-02 14:14:30 +0700

    Navbar Admin Pengaduan

---
+++ application/views/navbar_14.php
@@ -0,0 +1,9 @@
+<?php
+// Navbar Admin Pengaduan
+?>
+
+<div id="navbar" class="clearfloat">
+    <ul>
+        <li><?php echo anchor('admin_pengaduan/dashboard', 'Daftar Aduan', "class='current'");?></li>
+    </ul>
+</div>
--- application/views/navbar_admin_pengaduan.php
+++
@@ -1,5 +0,0 @@
-<div id="navbar" class="clearfloat">
-    <ul>
-        <li><?php echo anchor('admin_pengaduan/dashboard', 'Daftar Aduan', "class='current'");?></li>
-    </ul>
-</div>
