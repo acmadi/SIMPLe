a976173cad60a1625f8c1e42f0fcc55db8a20f6b
Mulia A. Nasution <mul14.net@gmail.com>
2012-02-07 17:45:06 +0700

    Perbaiki notifikasi karena gak bisa pake session.

--- application/helpers/notifikasi_helper.php
+++ application/helpers/notifikasi_helper.php
@@ -2,17 +2,18 @@
 
 function generate_notifkasi()
 {
-    if ($this->session->flashdata('success')) {
-        echo notification($this->session->flashdata('success'), 'Sukses', 'red');
+    $CI =& get_instance();
+    if ($CI->session->flashdata('success')) {
+        echo notification($CI->session->flashdata('success'), 'Sukses', 'green');
     }
-    if ($this->session->flashdata('info')) {
-        echo notification($this->session->flashdata('info'), 'Informasi', 'blue');
+    if ($CI->session->flashdata('info')) {
+        echo notification($CI->session->flashdata('info'), 'Informasi', 'blue');
     }
-    if ($this->session->flashdata('notice')) {
-        echo notification($this->session->flashdata('notice'), 'Perhatian', 'yellow');
+    if ($CI->session->flashdata('notice')) {
+        echo notification($CI->session->flashdata('notice'), 'Perhatian', 'yellow');
     }
-    if ($this->session->flashdata('error')) {
-        echo notification($this->session->flashdata('error'), 'Error', 'red');
+    if ($CI->session->flashdata('error')) {
+        echo notification($CI->session->flashdata('error'), 'Error', 'red');
     }
     if (validation_errors()) {
         echo notification(validation_errors(), 'Error', 'red');
