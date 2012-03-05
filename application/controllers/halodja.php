695c620030fab7d8c29825841cc30fb72ea49c52
Mulia A. Nasution <mul14.net@gmail.com>
2012-03-06 06:55:44 +0700

    Tambah Halo DJA

--- application/controllers/halodja.php
+++ application/controllers/halodja.php
@@ -17,4 +17,28 @@ class Halodja extends CI_Controller
         $data['content'] = '';
         $this->load->view('new-template', $data);
     }
+
+    public function status_tiket()
+    {
+        if ($_POST) {
+            $sql = "SELECT  no_tiket_frontdesk, a.id_unit, nama_unit, a.id_kementrian, nama_kementrian, keputusan, status, tanggal_selesai
+                    FROM tb_tiket_frontdesk a
+                    JOIN tb_kementrian b ON b.id_kementrian = a.id_kementrian
+                    JOIN tb_unit c ON c.id_unit = a.id_unit AND c.id_kementrian = a.id_kementrian
+                    WHERE a.no_tiket_frontdesk = ?";
+            $result = $this->db->query($sql, array($this->input->post('no_tiket')));
+
+            if ($result->num_rows() == 0) {
+                $this->session->set_flashdata('error', 'Tiket tidak ditemukan');
+                redirect('/halodja/status_tiket');
+            }
+
+            $data['tiket'] = $result->row();
+        }
+        $data['title'] = 'Cek Tiket';
+        $data['no_tiket'] = $this->input->post('no_tiket');
+        $data['content'] = 'halodja/cek_tiket';
+
+        $this->load->view('new-template', $data);
+    }
 }
---
+++ application/views/halodja/cek_tiket.php
@@ -0,0 +1,68 @@
+<div class="content">
+    <h1>Cek Tiket</h1>
+
+    <?php generate_notifkasi() ?>
+
+    <fieldset>
+        <legend>Pengecekan Tiket</legend>
+        <form method="post" action="<?php echo site_url('/halodja/status_tiket') ?>">
+            <p>
+                Nomer Tiket <input type="text" name="no_tiket" value="<?php echo $no_tiket;?>" autocomplete="off" />
+                <input type="submit" value="Cek Tiket" class="button green"/>
+                <a href="<?php echo site_url('halodja/status_tiket') ?>" class="button">Reset</a>
+            </p>
+        </form>
+    </fieldset>
+
+    <?php if (isset($tiket)): ?>
+    <fieldset>
+        <legend>Status Tiket</legend>
+        <table style="width: 100%;">
+            <tr>
+                <td>Nomer Tiket</td>
+                <td><strong>#<?php echo sprintf('%05d', $tiket->no_tiket_frontdesk) ?></strong></td>
+            </tr>
+
+            <tr>
+                <td>Kementerian</td>
+                <td><strong><?php echo $tiket->id_kementrian ?></strong> - <?php echo $tiket->nama_kementrian ?></td>
+            </tr>
+
+            <tr>
+                <td>Eselon</td>
+                <td><strong><?php echo $tiket->id_unit ?></strong> - <?php echo $tiket->nama_unit ?></td>
+            </tr>
+
+            <tr>
+                <td>Keputusan</td>
+                <td><strong>
+                    <?php
+                    if ($tiket->keputusan == '') {
+                        echo 'Diproses';
+                    } else {
+                        echo ucfirst($tiket->keputusan);
+                    }
+                    ?>
+                </strong></td>
+            </tr>
+
+            <tr>
+                <td>Tanggal Selesai</td>
+                <td><strong><?php echo ((!empty($tiket->tanggal_selesai))? strftime('%d %B %Y', strtotime($tiket->tanggal_selesai)):'');  ?></strong></td>
+            </tr>
+
+            <tr>
+                <td>Status </td>
+                <td><strong><?php echo ucfirst($tiket->status) ?></strong></td>
+            </tr>
+        </table>
+    </fieldset>
+
+    <?php elseif (isset($tiket) && $this->input->post()): ?>
+
+    Tiket tidak ditemukan
+
+    <?php endif ?>
+
+
+</div>
