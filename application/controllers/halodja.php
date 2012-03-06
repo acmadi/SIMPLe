3ab3c3af0d40e89876abd179dc40543a0cb71b8e
Mulia A. Nasution <mul14.net@gmail.com>
2012-03-06 15:06:27 +0700

    Halo DJA bisa mendapatkan informasi lebih mengenai tiket. Dan bisa liat bukti tanda terima pengajuan.

--- application/controllers/halodja.php
+++ application/controllers/halodja.php
@@ -18,22 +18,39 @@ class Halodja extends CI_Controller
         $this->load->view('new-template', $data);
     }
 
+    public function lihat_dokumen($nomor_tiket_frontdesk) {
+        redirect(base_url('output/pengajuan_' . $nomor_tiket_frontdesk . '.pdf'), null, false);
+    }
+
     public function status_tiket()
     {
         if ($_POST) {
-            $sql = "SELECT  no_tiket_frontdesk, a.id_unit, nama_unit, a.id_kementrian, nama_kementrian, keputusan, status, tanggal_selesai, nomor_surat_usulan
+            $sql = "SELECT no_tiket_frontdesk, a.id_unit, nama_unit, a.id_kementrian, nama_kementrian, keputusan,
+                           status, tanggal_selesai, nomor_surat_usulan, a.tanggal, d.nip
                     FROM tb_tiket_frontdesk a
                     JOIN tb_kementrian b ON b.id_kementrian = a.id_kementrian
                     JOIN tb_unit c ON c.id_unit = a.id_unit AND c.id_kementrian = a.id_kementrian
-                    WHERE a.no_tiket_frontdesk = ?";
-            $result = $this->db->query($sql, array($this->input->post('no_tiket')));
+                    JOIN tb_petugas_satker d ON a.id_petugas_satker = d.id_petugas_satker
+                    WHERE a.no_tiket_frontdesk LIKE ?
+                    OR a.nomor_surat_usulan LIKE ?
+                    OR d.nip LIKE ?
+                    OR b.nama_kementrian LIKE ?
+                    OR c.nama_unit LIKE ?
+                    ";
+            $result = $this->db->query($sql, array(
+                "%{$this->input->post('pencarian')}%",
+                "%{$this->input->post('pencarian')}%",
+                "%{$this->input->post('pencarian')}%",
+                "%{$this->input->post('pencarian')}%",
+                "%{$this->input->post('pencarian')}%",
+            ));
 
             if ($result->num_rows() == 0) {
                 $this->session->set_flashdata('error', 'Tiket tidak ditemukan');
                 redirect('/halodja/status_tiket');
             }
 
-            $data['tiket'] = $result->row();
+            $data['tikets'] = $result;
         }
         $data['title'] = 'Cek Tiket';
         $data['no_tiket'] = $this->input->post('no_tiket');
--- application/views/halodja/cek_tiket.php
+++ application/views/halodja/cek_tiket.php
@@ -7,61 +7,74 @@
         <legend>Pengecekan Tiket</legend>
         <form method="post" action="<?php echo site_url('/halodja/status_tiket') ?>">
             <p>
-                Nomor Tiket <input type="text" name="no_tiket" value="<?php echo $no_tiket;?>" autocomplete="off" />
+                Silahkan informasi yang berkaitan dengan tiket <br/>
+                <input type="text" name="pencarian" value="<?php echo $no_tiket;?>" autocomplete="off" />
                 <input type="submit" value="Cek Tiket" class="button green"/>
                 <a href="<?php echo site_url('halodja/status_tiket') ?>" class="button">Reset</a>
             </p>
         </form>
     </fieldset>
 
-    <?php if (isset($tiket)): ?>
-    <fieldset>
-        <legend>Status Tiket</legend>
-        <table style="width: 100%;">
+    <p>&nbsp;</p>
+
+    <?php if (isset($tikets)): ?>
+
+        <table class="table">
+            <thead>
             <tr>
-                <td>Nomor Tiket</td>
+                <th>Nomor Tiket</th>
+                <th>Nomor Surat Usulan</th>
+                <th>Kementerian</th>
+                <th>Eselon</th>
+                <th>Keputusan</th>
+                <th>Tanggal Masuk</th>
+                <th>Tanggal Selesai</th>
+                <th>Status</th>
+                <th>&nbsp;</th>
+            </tr>
+            </thead>
+            <tfoot>
+            <tr>
+                <td colspan="9">&nbsp;</td>
+            </tr>
+            </tfoot>
+            <tbody>
+
+            <?php foreach ($tikets->result() as $tiket): ?>
+
+            <tr>
                 <td><strong>#<?php echo sprintf('%05d', $tiket->no_tiket_frontdesk) ?></strong></td>
-            </tr>
-
-            <tr>
-                <td>Nomor Surat Usulan</td>
-                <td><strong><?php echo $tiket->nomor_surat_usulan ?></strong></td>
-            </tr>
-
-            <tr>
-                <td>Kementerian</td>
+                <td><?php echo $tiket->nomor_surat_usulan ?></td>
                 <td><strong><?php echo $tiket->id_kementrian ?></strong> - <?php echo $tiket->nama_kementrian ?></td>
-            </tr>
-
-            <tr>
-                <td>Eselon</td>
                 <td><strong><?php echo $tiket->id_unit ?></strong> - <?php echo $tiket->nama_unit ?></td>
+                <td>
+                    <strong>
+                        <?php
+                        if ($tiket->keputusan == '') {
+                            echo 'Diproses';
+                        } else {
+                            echo ucfirst($tiket->keputusan);
+                        }
+                        ?>
+                    </strong>
+                </td>
+                <td><?php echo ((!empty($tiket->tanggal))? strftime('%d %B %Y', strtotime($tiket->tanggal)):'');  ?></td>
+                <td><?php echo ((!empty($tiket->tanggal_selesai))? strftime('%d %B %Y', strtotime($tiket->tanggal_selesai)):'');  ?></td>
+                <td><?php echo ucfirst($tiket->status) ?></td>
+                <td class="action">
+                    <a href="<?php echo site_url('halodja/lihat_dokumen/' . $tiket->no_tiket_frontdesk) ?>"
+                        class="button green"
+                        target="_blank">
+                        Tanda Terima Pengajuan
+                    </a>
+                </td>
             </tr>
 
-            <tr>
-                <td>Keputusan</td>
-                <td><strong>
-                    <?php
-                    if ($tiket->keputusan == '') {
-                        echo 'Diproses';
-                    } else {
-                        echo ucfirst($tiket->keputusan);
-                    }
-                    ?>
-                </strong></td>
-            </tr>
+            <?php endforeach; ?>
 
-            <tr>
-                <td>Tanggal Selesai</td>
-                <td><strong><?php echo ((!empty($tiket->tanggal_selesai))? strftime('%d %B %Y', strtotime($tiket->tanggal_selesai)):'');  ?></strong></td>
-            </tr>
-
-            <tr>
-                <td>Status </td>
-                <td><strong><?php echo ucfirst($tiket->status) ?></strong></td>
-            </tr>
+            </tbody>
         </table>
-    </fieldset>
+
 
     <?php elseif (isset($tiket) && $this->input->post()): ?>
 
