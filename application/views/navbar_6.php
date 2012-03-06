016e627a84671399c4e1a499571c08c1ad5a6cb4
Mulia A. Nasution <mul14.net@gmail.com>
2012-03-06 14:03:36 +0700

    Ganti posisi menu Halo DJA.

--- application/views/navbar_6.php
+++ application/views/navbar_6.php
@@ -4,8 +4,8 @@ require_once(APPPATH . 'views/navbar_init.php');
 ?>
 
 <ul>
-    <li><?php echo anchor('helpdesks/identity', 'Helpdesk', "class='$nav_identitas_satker'");?></li>
     <li><?php echo anchor('halodja/status_tiket', 'Status Tiket', "class='$nav_status_tiket'");?></li>
+    <li><?php echo anchor('helpdesks/identity', 'Helpdesk', "class='$nav_identitas_satker'");?></li>
     <li><?php echo anchor('knowledge', 'Knowledge Base', "class='$nav_knowledge'");?></li>
     <li><?php echo anchor('referensi', 'Referensi Peraturan', "class='$nav_referensi'") ?></li>
     <li><?php echo anchor('forum', 'Forum', "class='$nav_forum'");?></li>
