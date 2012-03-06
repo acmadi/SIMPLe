71374d9002bcfb85382ca7a323498578da5bd5f3
Mulia A. Nasution <mul14.net@gmail.com>
2012-03-06 20:44:14 +0700

    New Admin - Daftar Kementerian

--- application/views/admin/kementrian.php
+++ application/views/admin/kementrian.php
@@ -1,42 +1,56 @@
-<div class="content">
+<div class="page-header">
+    <h2>Kementerian</h2>
+</div>
 
-    <h1>Kementerian</h1>
+<?php generate_notifkasi() ?>
 
-    <?php generate_notifkasi() ?>
+<div class="row-fluid">
+    <div class="span6">
+        <form method="get" action="<?php echo site_url('/admin/kementrian/index/') ?>">
+            <input type="text" name="cari" class="search-query"/>
+            <input type="submit" name="submit" value="Cari" class="btn"/>
+        </form>
+    </div>
 
-    <a href="<?php echo site_url('/admin/kementrian/add/') ?>" class="button blue-pill">Tambah Kementerian</a>
+    <div class="span6" style="text-align: right;">
+        <a href="<?php echo site_url('/admin/kementrian/add/') ?>" class="btn btn-primary">Tambah Kementerian</a>
+    </div>
+</div>
 
-    <form method="get" action="<?php echo site_url('/admin/kementrian/index/') ?>" style="text-align: right;">
-        <input type="text" name="cari"/>
-        <input type="submit" name="submit" value="Cari" class="button blue-pill"/>
-    </form>
+<table class="table">
+    <thead>
+    <tr>
+        <th class="no">No</th>
+        <th>Kementerian</th>
+        <th>&nbsp;</th>
+    </tr>
+    </thead>
+    <tfoot>
+    <tr>
+        <td colspan="10">&nbsp;</td>
+    </tr>
+    </tfoot>
+    <tbody>
+    <?php $i = 1 ?>
+    <?php foreach ($bla->result() as $value): ?>
+    <tr>
+        <td><?php echo $i++ ?></td>
+        <td><strong><?php echo $value->id_kementrian ?></strong> - <?php echo $value->nama_kementrian ?></a>
+        </td>
 
-    <table class="table">
-        <thead>
-        <tr>
-            <th class="no">No</th>
-            <th>Kementerian</th>
-            <th class="action">Aksi</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php $i = 1 ?>
-        <?php foreach ($bla->result() as $value): ?>
-        <tr>
-            <td class="no"><?php echo $i++ ?></td>
-            <td style=""><strong><?php echo $value->id_kementrian ?></strong> - <?php echo $value->nama_kementrian ?></a></td>
-            
-            <td class="action">
-                <a href="<?php echo site_url('/admin/kementrian/edit/' . $value->id_kementrian) ?>" class="button blue-pill">Edit</a>
-                <a href="<?php echo site_url('/admin/kementrian/delete/' . $value->id_kementrian) ?>" class="button gray-pill"
-                   onclick="return confirm('Anda yakin akan menghapus data ini?')">Hapus</a>
-            </td>
-        </tr>
+        <td class="action">
+            <a href="<?php echo site_url('/admin/kementrian/edit/' . $value->id_kementrian) ?>"
+               class="btn btn-info">Edit</a>
+            <a href="<?php echo site_url('/admin/kementrian/delete/' . $value->id_kementrian) ?>"
+               class="btn btn-danger"
+               onclick="return confirm('Anda yakin akan menghapus data ini?')">Hapus</a>
+        </td>
+    </tr>
         <?php endforeach ?>
 
-        </tbody>
-    </table>
+    </tbody>
+</table>
 
+<div class="pagination pagination-centered">
     <?php echo $this->pagination->create_links() ?>
-
 </div>
