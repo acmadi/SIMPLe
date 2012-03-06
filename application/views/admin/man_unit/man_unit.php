4d4c6c555955796f9d5a10b4c9af663404353aa1
Mulia A. Nasution <mul14.net@gmail.com>
2012-03-06 21:18:42 +0700

    New Admin - Manajemen Unit

--- application/views/admin/man_unit/man_unit.php
+++ application/views/admin/man_unit/man_unit.php
@@ -1,9 +1,3 @@
-	<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery-1.4.2.min.js"></script>
-	<script src="<?php echo base_url(); ?>js/jquery.ui.core.min.js"></script>
-	<script src="<?php echo base_url(); ?>js/jquery.ui.widget.min.js"></script>
-	<script src="<?php echo base_url(); ?>js/jquery.ui.position.min.js"></script>
-	<script src="<?php echo base_url(); ?>js/jquery.ui.autocomplete.min.js"></script>
-	<link rel="stylesheet" href="<?php echo base_url() . 'css/ui-lightness/jquery-ui-1.8.16.custom.css';?>"/>
 <style type="text/css">
     .ui-autocomplete {
         max-height: 300px;
@@ -14,80 +8,84 @@
         padding-right: 20px;
     }
 </style>
-<ul id="nav">
-    <li><a href="#tab1" class="active">Manajemen Unit</a></li>
-</ul>
-<div class="clear"></div>
-    <div id="konten">
-        <div style="display: none;" id="tab1" class="tab_konten">
 
-            <?php generate_notifkasi() ?>
+<div class="page-header">
+    <h2>Manajemen Unit</h2>
+</div>
 
-            <div class="table">
-                <div id="head">
-                    <div style="float: left;">
-                        <a class="button blue-pill" href="<?php echo site_url('/admin/man_unit_tambah') ?>">Tambah</a>
-                    </div>
+<?php generate_notifkasi() ?>
 
-                    <div style="float: right;">
-                        <form id="cari_unit" action="<?php echo site_url('/admin/man_unit_cari') ?>" method="post">
-                            Kode Unit: <input type="text" value="" placeholder="Pencarian kode unit" name="fcari" id="teks-cari" />
-                                <input class="button blue-pill" type="submit" value="Cari Unit" />
-                        </form>
-                    </div>
-                </div>
-                <div id="tail">
-                    <table id="tableOne" class="yui">
-                        <thead>
-                        <tr>
-                            <th>Kode Unit</th>
-                            <th>Nama Unit</th>
-                            <th class="action">Aksi</th>
-                        </tr>
-                        </thead>
-                        <tbody>
+<div class="row-fluid">
+    <div class="span6">
+        <form id="cari_unit" action="<?php echo site_url('/admin/man_unit_cari') ?>" method="post">
+            <input type="text" value="" placeholder="Pencarian kode unit" name="fcari" id="teks-cari"
+                   class="search-query"/>
+            <input class="btn" type="submit" value="Cari"/>
+        </form>
+    </div>
+    <div class="span6" style="text-align: right">
+        <a class="btn btn-primary" href="<?php echo site_url('/admin/man_unit_tambah') ?>">Tambah</a>
+    </div>
+</div>
 
 
-                        <?php foreach ($result as $unit): ?>
-                        <tr>
-                            <td><?php echo $unit->kode_unit ?></td>
-                            <td><?php echo $unit->nama_unit ?></td>
-                            <td class="action">
-                                <a href="<?php echo site_url('/admin/man_unit_ubah/index/' . $unit->id_unit_satker) ?>">
-                                    <img src="<?php echo base_url('images/edit.png') ?>" />
-                                </a>
-                                <a href="<?php echo site_url('/admin/man_unit/delete/' . $unit->id_unit_satker) ?>" onclick="return yesOrNo()">
-                                    <img src="<?php echo base_url('images/delete.png') ?>" />
-                                </a>
-                            </td>
-                        </tr>
-                        <?php endforeach ?>
+<table class="table">
+    <thead>
+    <tr>
+        <th>Kode Unit</th>
+        <th>Nama Unit</th>
+        <th>&nbsp;</th>
+    </tr>
+    </thead>
+    <tfoot>
+    <tr>
+        <td colspan="10">&nbsp;</td>
+    </tr>
+    </tfoot>
+    <tbody>
 
-                        </tbody>
-                    </table>
-                </div>
-                <div class="pagination"><?php echo ($pageLink)?'Halaman '.$pageLink:'';?></div><br />
-            </div>
-        </div>
+    <?php foreach ($result as $unit): ?>
+    <tr>
+        <td><?php echo $unit->kode_unit ?></td>
+        <td><?php echo $unit->nama_unit ?></td>
+        <td class="action">
+            <a href="<?php echo site_url('/admin/man_unit_ubah/index/' . $unit->id_unit_satker) ?>"
+               class="btn btn-info">
+                Ubah
+            </a>
+            <a href="<?php echo site_url('/admin/man_unit/delete/' . $unit->id_unit_satker) ?>"
+               onclick="return confirm('Anda yakin akan menghapus unit ini?')"
+               class="btn btn-danger">
+                Hapus
+            </a>
+        </td>
+    </tr>
+        <?php endforeach ?>
+
+    </tbody>
+</table>
+
+<div class="pagination pagination-centered"><?php echo ($pageLink) ? $pageLink : '' ?></div>
+
 <script type="text/javascript">
-    $(function() {
+    $(function () {
         $('#teks-cari').autocomplete({
-            source: function(request, response) {
+            source:function (request, response) {
                 $.ajax({
-                    url: "<?php echo site_url('/admin/man_unit/cari') ?>",
-                    data: {
-                        term: request.term
+                    url:"<?php echo site_url('/admin/man_unit/cari') ?>",
+                    data:{
+                        term:request.term
                     },
 
-                    dataType: 'json',
+                    dataType:'json',
 
-                    success: function(data) {
+                    success:function (data) {
                         response(data);
                     }
                 })
             },
-            delay: 500,
-            minLength: 1
+            delay:500,
+            minLength:1
         });
     });
 </script>
