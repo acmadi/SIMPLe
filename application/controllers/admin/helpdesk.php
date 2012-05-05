a8df356ee8db5fdc3ac729d0680b0bc8a5390fc8
Mulia A. Nasution <mul14.net@gmail.com>
2012-05-05 19:39:43 +0700

    Rapikan Helpdesk

--- application/controllers/admin/helpdesk.php
+++ application/controllers/admin/helpdesk.php
@@ -1,88 +1,86 @@
 <?php
 class Helpdesk extends CI_Controller
 {
+    public $title = 'Help Desk';
 
-    function __construct()
+    public function __construct()
     {
         parent::__construct();
         $this->load->model('mhelpdesk', 'helpdesk');
     }
 
-    var $title = 'Help Desk';
-
-    function index()
+    public function index()
     {
-	  $key = $this->input->post('fcari');
-      $value = $this->input->post('fkeyword');
-	  $page		= $this->helpdesk->get_all_tiket(); 
-	  $pageData	= $page['query']->result();
-	  $pageLink	= $page['pagination1'];
-	  $nomor	= $page['nomor_item'];
-	
-	  $data		= array('result'=>$pageData,'pageLink'=>$pageLink,'nomor'=>$nomor,);
-      $data['title'] = 'Help Desk';
-	  $data['pilihan'] = array(
-                  'tiket'  => 'No Tiket',
-                  'petugas'    => 'Nama Petugas',
-                  'satker'   => 'Nama Satker',
-                );
-	  $data['cari'] = $key;
-	  $data['keyword'] = $value;
-	  $nomor	= $page['nomor_item'];
-	  $data['content'] = 'admin/helpdesk/helpdesk';
+        $key = $this->input->post('fcari');
+        $value = $this->input->post('fkeyword');
+        $page = $this->helpdesk->get_all_tiket();
+        $pageData = $page['query']->result();
+        $pageLink = $page['pagination1'];
+        $nomor = $page['nomor_item'];
 
-    $bc[0]->link = 'admin/dashboard';
-    $bc[0]->label = 'Home';
-    $bc[1]->link = 'admin/helpdesk';
-    $bc[1]->label = 'Helpdesk';
-    $data['breadcrumb'] = $bc;
+        $data = array('result' => $pageData, 'pageLink' => $pageLink, 'nomor' => $nomor,);
+        $data['title'] = 'Help Desk';
+        $data['pilihan'] = array(
+            'tiket' => 'No Tiket',
+            'petugas' => 'Nama Petugas',
+            'satker' => 'Nama Satker',
+        );
+        $data['cari'] = $key;
+        $data['keyword'] = $value;
+        $nomor = $page['nomor_item'];
+        $data['content'] = 'admin/helpdesk/helpdesk';
 
-    $this->load->view('admin/template', $data);
+        $bc = array();
+        @$bc[0]->link = 'admin/dashboard';
+        @$bc[0]->label = 'Home';
+        @$bc[1]->link = 'admin/helpdesk';
+        @$bc[1]->label = 'Helpdesk';
+        $data['breadcrumb'] = $bc;
+
+        $this->load->view('admin/template', $data);
     }
-    function search()
+
+    public function search()
     {
-      /*if ($this->session->userdata('login') == TRUE)
+        /*if ($this->session->userdata('login') == TRUE)
         {*/
-      $key = $this->input->post('fcari');
-      $value = $this->input->post('fkeyword');
+        $key = $this->input->post('fcari');
+        $value = $this->input->post('fkeyword');
 
-	  $keyword = $key.'_'.$value;
-	    
-		$page		= $this->helpdesk->get_all_tiket_by_keyword($keyword);
-		$pageData	= $page['query']->result();
-		$pageLink	= $page['pagination1'];
-		$nomor	= $page['nomor_item'];
-		$cari	= $page['cari'];
-		$keyword	= $page['keyword'];
-		
-		
-		
-		$data		= array('result'=>$pageData,'pageLink'=>$pageLink,'nomor'=>$nomor,'cari'=>$cari,'keyword'=>$keyword,);
-		
-		$data['title'] 	 = 'Histori Cari';
-		$data['pilihan'] = array(
-                  'tiket'  => 'No Tiket',
-                  'petugas'    => 'Nama Petugas',
-                  'satker'   => 'Nama Satker',
-                );
-		$data['content'] = 'admin/helpdesk/helpdesk';
+        $keyword = $key . '_' . $value;
 
-    $bc[0]->link = 'admin/dashboard';
-    $bc[0]->label = 'Home';
-    $bc[1]->link = 'admin/helpdesk';
-    $bc[1]->label = 'Helpdesk';
-    $bc[2]->link = $this->uri->uri_string();
-    $bc[2]->label = 'Hasil Pencarian';
-    $data['breadcrumb'] = $bc;
+        $page = $this->helpdesk->get_all_tiket_by_keyword($keyword);
+        $pageData = $page['query']->result();
+        $pageLink = $page['pagination1'];
+        $nomor = $page['nomor_item'];
+        $cari = $page['cari'];
+        $keyword = $page['keyword'];
 
-		$this->load->view('admin/template', $data);
-	  
-      /*}
+
+        $data = array('result' => $pageData, 'pageLink' => $pageLink, 'nomor' => $nomor, 'cari' => $cari, 'keyword' => $keyword,);
+
+        $data['title'] = 'Histori Cari';
+        $data['pilihan'] = array(
+            'tiket' => 'No Tiket',
+            'petugas' => 'Nama Petugas',
+            'satker' => 'Nama Satker',
+        );
+        $data['content'] = 'admin/helpdesk/helpdesk';
+
+        $bc[0]->link = 'admin/dashboard';
+        $bc[0]->label = 'Home';
+        $bc[1]->link = 'admin/helpdesk';
+        $bc[1]->label = 'Helpdesk';
+        $bc[2]->link = $this->uri->uri_string();
+        $bc[2]->label = 'Hasil Pencarian';
+        $data['breadcrumb'] = $bc;
+
+        $this->load->view('admin/template', $data);
+
+        /*}
         else
         {
             $this->load->view('login/login_view');
         }*/
     }
 }
-
-?>
--- application/views/admin/helpdesk/helpdesk.php
+++ application/views/admin/helpdesk/helpdesk.php
@@ -1,74 +1,44 @@
+<?php generate_notifkasi() ?>
 
-	<div class="clear"></div>
-    <div id="" class="content">
-	<h1>Helpdesk</h1>
+<form action="<?php echo site_url('/admin/helpdesk/index') ?>" method="post" class="form-inline well">
 
-        <?php generate_notifkasi() ?>
+    <label class="control-label">Cari berdasarkan</label>
+    <?php echo form_dropdown('fcari', $pilihan, $cari, 'class="input-medium"');?>
 
-            <div class="table">
-                <div id="head">
-                    <div style="float: left;">
-                        <form id="cari_unit" action="<?php echo site_url('/admin/helpdesk/index') ?>" method="post">
-							<table>
-								<tr>
-									<td>
-									Cari Berdasarkan
-									</td>
-									<td>
-										<?php echo form_dropdown('fcari', $pilihan, $cari);?>
-									</td>
-									<td>
-									</td>
-								</tr>
-								<tr>
-									<td>
-									Keyword
-									</td>
-									<td>
-										<input type="text" value="<?php echo $keyword;?>" placeholder="Keyword" name="fkeyword" id="teks-cari" />
-										
-									</td>
-									<td>
-										<input class="button blue-pill" type="submit" value="Cari Helpdesk" />
-									</td>
-								</tr>
-							</table>
-                            
-                        </form>
-                    </div>
-                </div>
-                <div id="tail">
-                    <table id="tableOne" class="yui">
-                        <thead>
-                        <tr>
-                            <th style="width: 20px">No</th>
-                            <th style="width: 30px">Tanggal</th>
-                            <th style="width: 50px">No. Tiket</th>
-                            <th style="width: 150px">Nama Satker</th>
-                            <th>Pertanyaan</th>
-                            <th style="width: 20px">Status</th>
-                        </tr>
-                        </thead>
-                        <tbody>
+    <label class="control-label">Keyword</label>
+    <input type="text" value="<?php echo $keyword;?>" placeholder="Keyword" name="fkeyword" id="teks-cari"/>
+
+    <button type="submit" class="btn input-query" value="Cari"/>
+    <i class="icon-search"></i></button>
+
+</form>
 
 
-                        <?php $i = $nomor +1; foreach ($result as $tiket): ?>
-                        <tr>
-                            <td style="width: 20px"><?php echo $i++ ?></td>
-                            <td style="width: 30px"><?php echo $tiket->tanggal ?></td>
-                            <td style="width: 50px"><?php echo $tiket->no_tiket_helpdesk ?></td>
-                            <td style="width: 150px"><?php echo $tiket->nama_satker ?></td>
-                            <td><?php echo $tiket->pertanyaan ?></td>
-                            <td style="width: 20px"><?php echo $tiket->status ?></td>
-                            
-                        </tr>
-                        <?php endforeach ?>
+<table class="table">
+    <thead>
+    <tr>
+        <th>No. Tiket</th>
+        <th>Tanggal</th>
+        <th>Nama Satker</th>
+        <th>Pertanyaan</th>
+        <th>Status</th>
+    </tr>
+    </thead>
+    <tbody>
 
-                        </tbody>
-                    </table>
-                </div>
-                <div class="pagination"><?php echo ($pageLink)?'Halaman '.$pageLink:'';?></div><br />
-            </div>
-    </div>
 
-<script type="text/javascript" src="<?php echo base_url(); ?>js/fungsi.js"></script>
+    <?php foreach ($result as $tiket): ?>
+    <tr>
+        <td><?php echo sprintf('#%05d', $tiket->no_tiket_helpdesk) ?></td>
+        <td><?php echo $tiket->tanggal ?></td>
+        <td><?php echo $tiket->nama_satker ?></td>
+        <td><?php echo $tiket->pertanyaan ?></td>
+        <td><?php echo $tiket->status ?></td>
+
+    </tr>
+        <?php endforeach ?>
+
+    </tbody>
+</table>
+
+<div class="pagination"><?php echo ($pageLink) ? 'Halaman ' . $pageLink : '';?></div>
