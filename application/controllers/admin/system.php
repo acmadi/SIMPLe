399f3b74b6fa73beceadd88e136c914210cbc9b9
Mulia A. Nasution <mul14.net@gmail.com>
2012-05-06 14:07:27 +0700

    Bagusin Backup

--- application/controllers/admin/system.php
+++ application/controllers/admin/system.php
@@ -1,18 +1,22 @@
 <?php
-class System extends CI_Controller {
-    public function __construct() {
+class System extends CI_Controller
+{
+    public function __construct()
+    {
         parent::__construct();
     }
 
-    public function index() {
+    public function index()
+    {
         $data['title'] = 'System';
         $data['content'] = 'admin/system';
 
-        $bc[0]->link = 'admin/dashboard';
-	    $bc[0]->label = 'Home';
-	    $bc[1]->link = 'admin/system';
-	    $bc[1]->label = 'Sistem';
-	    $data['breadcrumb'] = $bc;
+        $bc = array();
+        @$bc[0]->link = 'admin/dashboard';
+        @$bc[0]->label = 'Home';
+        @$bc[1]->link = 'admin/system';
+        @$bc[1]->label = 'Sistem';
+        $data['breadcrumb'] = $bc;
 
         $this->load->view('admin/template', $data);
     }
--- application/views/admin/system.php
+++ application/views/admin/system.php
@@ -2,5 +2,9 @@
     <h2>Administrasi Sistem</h2>
 </div>
 
-<a class="btn btn-primary" href="<?php echo site_url('admin/dashboard/database_backup') ?>">Database Backup</a>
+<a class="btn btn-primary"
+   href="<?php echo site_url('admin/dashboard/database_backup') ?>">
+    <i class="icon-hdd icon-white"></i>
+    Database Backup
+</a>
 
